b1f34592ae6ac14f3af5b497350215335c6d3384
Christoph Hombach <Christoph.Hombach@2xideas.com>
2023-04-09 21:32:33 +0200

    Setup MAriaDB

--- montrek/montrek/settings.py
+++ montrek/montrek/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'montrek.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'montrek_db',
+        'USER': 'montrekuser',
+        'PASSWORD': 'X5e%85~-Xs',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
