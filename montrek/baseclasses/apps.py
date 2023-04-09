51daaf0940ede893ab2195a687713f6e5b6f26d6
Christoph Hombach <Christoph.Hombach@2xideas.com>
2023-04-09 21:59:55 +0200

    Add base Data ABCs

---
+++ montrek/baseclasses/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BaseclassesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'baseclasses'
---
+++ montrek/baseclasses/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+#Base Hub Model ABC
+class MontrekHubABC(models.Model):
+    class Meta:
+        abstract = True
+
+    name = models.CharField(max_length=12)
+
+
+#Base Satellite Model ABC
+class MontrekSatelliteABC(models.Model):
+    class Meta:
+        abstract = True
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+#Base Link Model ABC
+class MontrekLinkABC(models.Model):
+    class Meta:
+        abstract = True
