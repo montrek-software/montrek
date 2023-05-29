17064c00ef8634bb9487dfcb9926b85e03b2cfb1
Christoph Hombach <Christoph.Hombach@2xideas.com>
2023-05-29 13:32:07 +0200

    Add Account Credit Institute Link

--- montrek/link_tables/models.py
+++ montrek/link_tables/models.py
@@ -2,8 +2,21 @@ from django.db import models
 from baseclasses import models as baseclass_models
 from account import models as account_models
 from transaction import models as transaction_models
+from credit_institution import models as credit_institution_models
 
 # Create your models here.
 class AccountTransactionLink(baseclass_models.MontrekLinkABC):
-    from_hub = models.ForeignKey(account_models.AccountHub, on_delete=models.CASCADE, related_name='from_hub')
-    to_hub = models.ForeignKey(transaction_models.TransactionHub, on_delete=models.CASCADE, related_name='to_hub')
+    from_hub = models.ForeignKey(account_models.AccountHub,
+                                 on_delete=models.CASCADE,
+                                 related_name='account_transaction_link_from_hub')
+    to_hub = models.ForeignKey(transaction_models.TransactionHub, 
+                               on_delete=models.CASCADE, 
+                               related_name='account_transaction_to_hub')
+
+class AccountCreditInstituteLink(baseclass_models.MontrekLinkABC):
+    from_hub = models.ForeignKey(account_models.AccountHub,
+                                 on_delete=models.CASCADE,
+                                 related_name='account_credit_institute_link_from_hub')
+    to_hub = models.ForeignKey(credit_institution_models.CreditInstitutionHub, 
+                               on_delete=models.CASCADE, 
+                               related_name='account_credit_institute_link_to_hub')
