74290e0786aec0d1a113187ab3fc1ee8edb57cef
Christoph Hombach <Christoph.Hombach@2xideas.com>
2023-06-10 07:54:57 +0200

    Add fields to Satellite

--- montrek/file_upload/models.py
+++ montrek/file_upload/models.py
@@ -1,3 +1,5 @@
+import re
+
 from django.db import models
 
 from baseclasses import models as baseclass_models
@@ -7,4 +9,33 @@ class FileUploadRegistryHub(baseclass_models.MontrekHubABC):
     pass
 
 class FileUploadRegistryStaticSatellite(baseclass_models.MontrekSatelliteABC):
-    hub_entity = models.ForeignKey(FileUploadRegistryHub, on_delete=models.CASCADE)
+    class FileTypes(models.TextChoices):
+        CSV = 'csv'
+        JSON = 'json'
+        XML = 'xml'
+        XLSX = 'xlsx'
+        XLS = 'xls'
+        TXT = 'txt'
+        NONE = 'none'
+
+    hub_entity = models.ForeignKey(FileUploadRegistryHub, 
+                                   on_delete=models.CASCADE)
+    file_name = models.CharField(max_length=255)
+    file_path = models.CharField(max_length=255)
+    file_type = models.CharField(max_length=5, 
+                                 choices=FileTypes.choices,
+                                 default=FileTypes.NONE)
+
+    def clean(self):
+        super().clean()
+        if not self.file_name.endswith(f'.{self.file_type}'):
+            raise ValidationError('File name does not match file type')
+        # Regular expression pattern to validate the path structure
+        pattern = r'^[a-zA-Z0-9_/\\-]*$'
+        if not re.match(pattern, self.file_path):
+            raise ValidationError('File path is not valid')
+
+    def save(self, *args, **kwargs):
+        if self.file_type == self.FileTypes.NONE and '.' in self.file_name:
+            self.file_type = self.file_name.split('.')[-1]
+        super().save(*args, **kwargs)
--- montrek/file_upload/tests/tests_models.py
+++ montrek/file_upload/tests/tests_models.py
@@ -6,6 +6,9 @@ from file_upload.tests.factories.file_upload_factories import (
 from file_upload.models import (
     FileUploadRegistryStaticSatellite
 )
+from file_upload.models import (
+    FileUploadRegistryHub
+)
 
 class TestFileUploadModels(TestCase):
     @classmethod
@@ -13,4 +16,24 @@ class TestFileUploadModels(TestCase):
         FileUploadRegistryStaticSatelliteFactory.create_batch(3)
 
     def test_file_upload_registry_static_satellite(self):
-        self.assertEqual(FileUploadRegistryStaticSatellite.objects.count(), 3)
+        for file_type in ('csv', 'json', 'xml', 'xlsx', 'xls', 'txt'):
+            fu_hub = FileUploadRegistryHub.objects.create()
+            file_upload_registry_static_satellite = (
+                FileUploadRegistryStaticSatellite.objects.create(
+                    hub_entity = fu_hub,
+                    file_name = f'test_file_name.{file_type}',
+                    file_path = 'test/file/path',
+                    ))
+            self.assertEqual(file_upload_registry_static_satellite.file_name, f'test_file_name.{file_type}')
+            self.assertEqual(file_upload_registry_static_satellite.file_path, 'test/file/path')
+            self.assertEqual(file_upload_registry_static_satellite.file_type, file_type)
+
+    def test_file_upload_registry_static_satellite_type_none(self):
+        fu_hub = FileUploadRegistryHub.objects.create()
+        file_upload_registry_static_satellite = (
+            FileUploadRegistryStaticSatellite.objects.create(
+                hub_entity = fu_hub,
+                file_name = 'test_file_name',
+                file_path = 'test/file/path',
+                ))
+        self.assertEqual(file_upload_registry_static_satellite.file_type, 'none')
