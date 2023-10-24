f26b256a72d54b012ea2fc6a67d9e16b8d27ca80
Christoph Hombach <Christoph.Hombach@2xideas.com>
2023-10-24 14:55:50 +0200

    ISIN and WKN validators

--- montrek/asset/models.py
+++ montrek/asset/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from baseclasses import models as baseclass_models
+from asset.managers.validators import montrek_isin_validator
+from asset.managers.validators import montrek_wkn_validator
 
 # Create your models here.
 
@@ -14,10 +16,8 @@ class AssetStaticSatellite(baseclass_models.MontrekSatelliteABC):
 class AssetLiquidSatellite(baseclass_models.MontrekSatelliteABC):
     hub_entity = models.ForeignKey(AssetHub, on_delete=models.CASCADE, related_name="liquid_satellites")
     identifier_fields = ["asset_isin", "asset_wkn"]
-    #TODO ISIN Validator
-    asset_isin = models.CharField(max_length=100)
-    #TODO Add asset Type
-    asset_wkn = models.CharField(max_length=100)
+    asset_isin = models.CharField(max_length=12,validators=[montrek_isin_validator])
+    asset_wkn = models.CharField(max_length=6, validators=[montrek_wkn_validator])
 
 class AssetTimeSeriesSatellite(baseclass_models.MontrekSatelliteABC):
     hub_entity = models.ForeignKey(AssetHub, on_delete=models.CASCADE, related_name="time_series_satellites")
