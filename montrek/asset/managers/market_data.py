785d5c4ca16ef86478806eab1b73a7aaabb3616f
Christoph Hombach <Christoph.Hombach@2xideas.com>
2023-10-27 13:58:58 +0200

    Get Prices from yahoo

--- montrek/asset/managers/market_data.py
+++ montrek/asset/managers/market_data.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import pandas as pd
 from typing import Dict
 from asset.models import (
     AssetLiquidSatellite,
@@ -11,12 +12,15 @@ from baseclasses.repositories import db_helper
 def get_isin_asset_map() -> Dict[str, int]:
     return {asset.asset_isin: asset.hub_entity.id  for asset in AssetLiquidSatellite.objects.all()} 
 
-def update_asset_prices(isin_asset_map: Dict[str, int]):
-    price_data = yf.download(list(asset_isins.keys()), period="1d") 
+
+def get_yf_prices_per_isin(isins_list: list) -> pd.DataFrame:
+    price_data = yf.download(isins_list, period="1d") 
     price_data = price_data.stack(level=1).rename_axis(['Date', 'Ticker']).reset_index(level=1)
-    price_data = price_data.loc[:, ['Ticker','Close']]
+    return price_data.loc[:, ['Ticker','Close']]
+
+def update_asset_prices(isin_asset_map: Dict[str, int], price_data: pd.DataFrame):
     for per_date, row in price_data.iterrows():
-        asset_hub = AssetHub.objects.get(id=asset_isins[row['Ticker']])
+        asset_hub = AssetHub.objects.get(id=isin_asset_map[row['Ticker']])
         db_helper.new_satellite_entry(
             AssetTimeSeriesSatellite,
             asset_hub,
--- montrek/asset/tests/managers/test_market_data.py
+++ montrek/asset/tests/managers/test_market_data.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+import pandas as pd
 from django.test import TestCase
 from asset.tests.factories.asset_factories import (
     AssetStaticSatelliteFactory,
@@ -6,6 +8,7 @@ from asset.tests.factories.asset_factories import (
 from asset.models import AssetHub
 from asset.managers.market_data import update_asset_prices
 from asset.managers.market_data import get_isin_asset_map
+from asset.managers.market_data import get_yf_prices_per_isin
 
 class TestMarketData(TestCase):
     @classmethod
@@ -39,10 +42,22 @@ class TestMarketData(TestCase):
                              {'IE00BYZK4669': self.asset_1.hub_entity.id,
                               'US0378331005': self.asset_2.hub_entity.id})
 
+    @patch('asset.managers.market_data.yf.download')
+    def test_get_yf_prices_per_isin(self, mock_download):
+        mock_data_df = self._get_yf_prices_mock_data()
+        mock_download.return_value = mock_data_df
+        isins_list = ['IE00BYZK4669', 'US0378331005']
+        prices_df = get_yf_prices_per_isin(isins_list)
+        expected_df = self._dummy_yf_prices_df()
+        pd.testing.assert_frame_equal(prices_df, expected_df)
+
+
     def test_get_market_data(self):
         isin_asset_map = get_isin_asset_map()
-        update_asset_prices(isin_asset_map)
-        for asset in AssetHub.objects.all():
+        prices_df = self._dummy_yf_prices_df()
+        update_asset_prices(isin_asset_map, prices_df)
+        prices = [5.94, 166.89]
+        for i, asset in enumerate(AssetHub.objects.all()):
             asset_static = asset.asset_static_satellite.last()
             asset_ts = asset.asset_time_series_satellite.all()
             if not asset_static.is_liquid:
@@ -50,6 +65,30 @@ class TestMarketData(TestCase):
                 continue
             self.assertEqual(asset_ts.count(), 1)
             asset_ts = asset_ts.first()
-            self.assertEqual(asset_ts.price, 100)
+            self.assertAlmostEqual(float(asset_ts.price), prices[i])
 
+    def _get_yf_prices_mock_data(self):
+        data = {
+            ('Adj Close', 'IE00BYZK4669'): [None, 5.9425],
+            ('Adj Close', 'US0378331005'): [166.889999, None],
+            ('Close', 'IE00BYZK4669'): [None, 5.9425],
+            ('Close', 'US0378331005'): [166.889999, None],
+            ('High', 'IE00BYZK4669'): [None, 5.9575],
+            ('High', 'US0378331005'): [171.380005, None],
+            ('Low', 'IE00BYZK4669'): [None, 5.905],
+            ('Low', 'US0378331005'): [165.669998, None],
+            ('Open', 'IE00BYZK4669'): [None, 5.905],
+            ('Open', 'US0378331005'): [170.369995, None],
+            ('Volume', 'IE00BYZK4669'): [None, 9316.0],
+            ('Volume', 'US0378331005'): [70345400.0, None]
+        }
 
+        index = pd.DatetimeIndex(['2023-10-26', '2023-10-27'], name='Date')
+        return pd.DataFrame(data, index=index)
+
+    def _dummy_yf_prices_df(self):
+        return pd.DataFrame(
+            {'Ticker': ['US0378331005','IE00BYZK4669'],
+             'Close': [166.889999, 5.942500]},
+            index=pd.DatetimeIndex(['2023-10-26', '2023-10-27'], name='Date')
+        )
