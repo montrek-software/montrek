01ffce9a59af9d66e7c153afb5f821f1dcebd875
Christoph Hombach <Christoph.Hombach@2xideas.com>
2023-11-27 10:04:10 +0100

    Set ActionElement!

--- montrek/credit_institution/pages.py
+++ montrek/credit_institution/pages.py
@@ -2,20 +2,20 @@ from django.urls import reverse
 from baseclasses.dataclasses.view_classes import TabElement, ActionElement
 from baseclasses.pages import MontrekPage
 
-#action_create = ActionElement(
-#    icon="plus",
-#    link=reverse("credit_institution_create"),
-#    action_id="create_credit_institution",
-#    hover_text="Create Credit Institution",
-#)
-#
+action_create = ActionElement(
+    icon="plus",
+    #link=reverse("credit_institution_create"),
+    link="",
+    action_id="create_credit_institution",
+    hover_text="Create Credit Institution",
+)
+
 overview_tab = TabElement(
     name="Overview",
-    #link=reverse("credit_instritution"),
+    #link=reverse("account"),
     link="",
     html_id="tab_overview",
-    #actions=(action_create),
-    actions=(),
+    actions=(action_create,),
 )
 
 
