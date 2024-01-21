866cb93348b1cfd88084a29fd9a31efd8533dce4
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-01-21 08:43:43 +0100

    Fix test

--- montrek/account/tests/test_regression.py
+++ montrek/account/tests/test_regression.py
@@ -9,10 +9,16 @@ from credit_institution.tests.factories.credit_institution_factories import (
 from baseclasses.repositories.db_creator import DbCreator
 from transaction.models import TransactionHub, TransactionSatellite
 from transaction.repositories.transaction_repository import TransactionRepository
-from transaction.tests.factories.transaction_factories import TransactionCategorySatelliteFactory 
+from transaction.tests.factories.transaction_factories import (
+    TransactionCategorySatelliteFactory,
+)
+
 
 class AccountRegressionTests(TestCase):
-    test_csv_path = os.path.join(os.path.dirname(__file__), "managers/data/dkb_test.csv")
+    test_csv_path = os.path.join(
+        os.path.dirname(__file__), "managers/data/dkb_test.csv"
+    )
+
     @classmethod
     def setUpTestData(cls):
         cls.bank_account = BankAccountStaticSatelliteFactory.create()
@@ -20,10 +26,13 @@ class AccountRegressionTests(TestCase):
             hub_entity=cls.bank_account.hub_entity,
             account_type="BankAccount",
         )
-        cls.credit_institution = CreditInstitutionStaticSatelliteFactory.create(account_upload_method="dkb")
+        cls.credit_institution = CreditInstitutionStaticSatelliteFactory.create(
+            account_upload_method="dkb"
+        )
         cls.bank_account.hub_entity.link_account_credit_institution.add(
             cls.credit_institution.hub_entity
         )
+
     def test_upload_dkb_transactions_and_change_transaction_category(self):
         transactions = upload_dkb_transactions(
             self.bank_account.hub_entity, self.test_csv_path
@@ -35,8 +44,11 @@ class AccountRegressionTests(TestCase):
         test_data = repository.object_to_dict(query.first())
 
         test_category = TransactionCategorySatelliteFactory.create(typename="TestCat")
-        test_data['link_transaction_transaction_category'] = test_category.hub_entity
-        creator = DbCreator(hub_entity_class = TransactionHub, satellite_classes=[TransactionSatellite])
+        test_data["link_transaction_transaction_category"] = test_category.hub_entity
+        creator = DbCreator(
+            hub_entity_class=TransactionHub, satellite_classes=[TransactionSatellite]
+        )
         creator.create(test_data, TransactionHub())
+        creator.save_stalled_objects()
         self.assertEqual(len(transactions), 14)
         self.assertEqual(query.first().typename, "TestCat")
