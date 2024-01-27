44e5d5a82a498b53671a7c05cfad61535ef34b45
chrishombach <chris.hombach@gmail.com>
2024-01-27 19:45:04 +0100

    Fix empty auftraggeber (#120)
    Co-authored-by: Christoph Hombach <Christoph.Hombach@2xideas.com>

--- montrek/account/managers/transaction_upload_methods.py
+++ montrek/account/managers/transaction_upload_methods.py
@@ -62,6 +62,7 @@ def read_dkb_transactions_from_csv(file_path: str) -> pd.DataFrame:
         "transaction_price": lambda x: x.sum(),
     }
     transaction_df['transaction_description'] = transaction_df['transaction_description'].astype(str)
+    transaction_df['transaction_party'] = transaction_df['transaction_party'].fillna("UNKNOWN")
     transaction_df = (
         transaction_df.groupby(
             ["transaction_date", "transaction_party", "transaction_party_iban"]
