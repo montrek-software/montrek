fe870208abc29cb828e41903582fec1b700fa6de
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-01-28 08:40:19 +0100

    Init File Upload

--- montrek/file_upload/managers/file_upload_manager.py
+++ montrek/file_upload/managers/file_upload_manager.py
@@ -1,17 +1,30 @@
 from typing import TextIO
 from typing import Protocol
+from file_upload.repositories.file_upload_registry_repository import (
+    FileUploadRegistryRepository,
+)
 
 
 class FileUploadProcessorProtocol(Protocol):
-    def process(self, file: TextIO) -> None:
+    def process(self, file: TextIO) -> bool:
         ...
 
 
 class FileUploadManager:
     def __init__(
-        self, file_upload_processor: FileUploadProcessorProtocol, file_path: str
+        self, file_upload_processor: FileUploadProcessorProtocol, file: TextIO
     ) -> None:
         self.file_upload_processor = file_upload_processor
+        self.repository = FileUploadRegistryRepository()
+        self.file = file
+        self.file_upload_registry = None
 
     # def upload(self, file: TextIO) -> None:
     #    self.file_upload_processor.process(file)
+
+    def init_upload(self) -> None:
+        file_name = self.file.name
+        file_type = file_name.split(".")[-1]
+        self.file_upload_registry = self.repository.std_create_object(
+            {"file_name": file_name, "file_type": file_type, "upload_status": "pending"}
+        )
--- montrek/file_upload/tests/managers/test_file_upload_manager.py
+++ montrek/file_upload/tests/managers/test_file_upload_manager.py
@@ -1,20 +1,30 @@
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
 from file_upload.managers.file_upload_manager import FileUploadManager
+from file_upload.repositories.file_upload_registry_repository import (
+    FileUploadRegistryRepository,
+)
+
+class MockFileUploadProcessor:
+    def process(self, file):
+        return True
 
 class TestFileUploadManager(TestCase):
     def setUp(self):
-        class MockFileUploadProcessor:
-            def process(self, file):
-                pass
-        self.file_upload_processor = MockFileUploadProcessor()
-        self.test_file_path = "/tmp/test_file.txt"
-        with open(self.test_file_path, "w") as f:
-            f.write("test")
-        f.close()
+        self.test_file = SimpleUploadedFile(
+            name="test_file.txt",
+            content="test".encode("utf-8"),
+            content_type="text/plain",
+        )
 
     def test_fum_init(self):
         fum = FileUploadManager(
-            file_upload_processor=self.file_upload_processor,
-            file_path=self.test_file_path
+            file_upload_processor=MockFileUploadProcessor(), file=self.test_file
         )
+        fum.init_upload()
+        file_upload_registry_query = FileUploadRegistryRepository().std_queryset()
+        self.assertEqual(file_upload_registry_query.count(), 1)
+        file_upload_registry = file_upload_registry_query.first()
+        self.assertEqual(file_upload_registry.file_name, "test_file.txt")
+        self.assertEqual(file_upload_registry.file_type, "txt")
+        self.assertEqual(file_upload_registry.upload_status, "pending")
