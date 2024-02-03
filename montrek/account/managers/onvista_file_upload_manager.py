a2266d3b3b0a3c8306496d34b5843b8e652d593d
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-02-03 20:49:16 +0100

    Setup subprocessor

--- montrek/account/managers/onvista_file_upload_manager.py
+++ montrek/account/managers/onvista_file_upload_manager.py
@@ -8,13 +8,21 @@ class OnvistaFileUploadProcessor:
         self.account_hub = account_hub
         self.subprocessor = NotImplementedFileUploadProcessor()
 
-    def pre_check(self, file_path: str):
+    def pre_check(self, file_path: str) -> bool:
         self._get_subprocessor(file_path)
         return self.subprocessor.pre_check(file_path)
 
     def _get_subprocessor(self, file_path: str):
-        ...
+        index_tag = open(file_path).readline().strip()
+        if index_tag.startswith("Depotuebersicht"):
+            self.subprocessor = OnvistaFileUploadDepotProcessor()
+            return
+        self.subprocessor = NotImplementedFileUploadProcessor()
+        self.message = "File cannot be processed"
 
 
 class OnvistaFileUploadDepotProcessor:
     message = "Not implemented"
+
+    def pre_check(self, file_path: str) -> bool:
+        return True
--- montrek/account/tests/managers/test_onvista_file_upload_manager.py
+++ montrek/account/tests/managers/test_onvista_file_upload_manager.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
-from account.managers.onvista_file_upload_manager import OnvistaFileUploadProcessor
+from account.managers.onvista_file_upload_manager import (
+    OnvistaFileUploadDepotProcessor,
+    OnvistaFileUploadProcessor,
+)
+from account.managers.not_implemented_processor import NotImplementedFileUploadProcessor
 
 
 class TestOnvistaFileUploadManager(TestCase):
@@ -7,4 +11,31 @@ class TestOnvistaFileUploadManager(TestCase):
         processor = OnvistaFileUploadProcessor(None)
         self.assertEqual(processor.message, "Not implemented")
 
-        self.assertEqual(processor.pre_check("test"), False)
+    def test_no_type_detectable(self):
+        processor = OnvistaFileUploadProcessor(None)
+        test_path = "/tmp/test_file.csv"
+        with open(test_path, "w") as f:
+            f.write("UNKNOWN")
+        result = processor.pre_check(test_path)
+        self.assertEqual(result, False)
+        self.assertEqual(processor.message, "File cannot be processed")
+        self.assertIsInstance(processor.subprocessor, NotImplementedFileUploadProcessor)
+
+    def test_empty_file(self):
+        processor = OnvistaFileUploadProcessor(None)
+        test_path = "/tmp/test_file.csv"
+        with open(test_path, "w"):
+            ...
+        result = processor.pre_check(test_path)
+        self.assertEqual(result, False)
+        self.assertEqual(processor.message, "File cannot be processed")
+        self.assertIsInstance(processor.subprocessor, NotImplementedFileUploadProcessor)
+
+    def test_depotuebersicht_processor(self):
+        processor = OnvistaFileUploadProcessor(None)
+        test_path = "/tmp/test_file.csv"
+        with open(test_path, "w") as f:
+            f.write("Depotuebersicht Wertpapiere")
+        result = processor.pre_check(test_path)
+        self.assertEqual(result, True)
+        self.assertIsInstance(processor.subprocessor, OnvistaFileUploadDepotProcessor)
