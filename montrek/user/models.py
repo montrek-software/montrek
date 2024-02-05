c0088246ca70924a7dc2ef252517be2b3e68f7ea
Vincent Mohiuddin <vincent.mohiuddin@2xideas.com>
2024-02-05 19:03:39 +0100

    use email as username

--- montrek/user/models.py
+++ montrek/user/models.py
@@ -1,9 +1,34 @@
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-class MontrekUserManager(UserManager):
-    pass
+
+class MontrekUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("User must have an email address.")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class MontrekUser(AbstractUser):
+    username = None
+    email = models.EmailField(("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
     objects = MontrekUserManager()
+
+    def __str__(self):
+        return self.email
 
