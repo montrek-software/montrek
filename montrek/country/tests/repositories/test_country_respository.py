8e01e2e6c0a39ce6b020fbc971e8820aa52403d1
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-02-07 21:08:29 +0100

    Add tests

--- montrek/country/tests/repositories/test_country_respository.py
+++ montrek/country/tests/repositories/test_country_respository.py
@@ -17,3 +17,19 @@ class CountryRepositoryTest(TestCase):
             self.assertEqual(
                 test_countries[i].country_code, self.test_countries[i].country_code
             )
+
+    def test_create_data(self):
+        input_data = {"country_name": "TestCountry", "country_code": "TST"}
+        repository = CountryRepository()
+        repository.std_create_object(input_data)
+        test_countries = repository.std_queryset()
+        self.assertEqual(len(test_countries), 4)
+        self.assertEqual(test_countries[3].country_name, "TestCountry")
+        self.assertEqual(test_countries[3].country_code, "TST")
+        input_data = {"country_name": "UnitedTestCountry", "country_code": "TST"}
+        repository = CountryRepository()
+        repository.std_create_object(input_data)
+        test_countries = repository.std_queryset()
+        self.assertEqual(len(test_countries), 4)
+        self.assertEqual(test_countries[3].country_name, "UnitedTestCountry")
+        self.assertEqual(test_countries[3].country_code, "TST")
