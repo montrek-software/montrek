57c1b455f04777d5467f7e6908a1506ea0072ffd
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-02-07 20:42:21 +0100

    Include models

---
+++ montrek/country/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from baseclasses import models as baseclass_models
+
+# Create your models here.
+
+
+class CountryHub(baseclass_models.MontrekHubABC):
+    pass
+
+
+class CountryStaticSatellite(baseclass_models.MontrekSatelliteABC):
+    hub_entity = models.ForeignKey(CountryHub, on_delete=models.CASCADE)
+    country_name = models.CharField(max_length=255)
+    country_code = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.country_name
