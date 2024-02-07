fa21d6230aa61e4938e944de119113117ce89cdc
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-02-07 20:24:29 +0100

    Removed unneed depot tables

--- montrek/depot/tests/factories/depot_factories.py
+++ montrek/depot/tests/factories/depot_factories.py
@@ -1,11 +1,1 @@
 import factory
-
-class DepotHubFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = 'depot.DepotHub'
-
-class DepotSatelitteFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = 'depot.DepotSatelitte'
-    hub_entity = factory.SubFactory(DepotHubFactory)
-    depot_name = factory.Sequence(lambda n: f'depot_name_{n}')
