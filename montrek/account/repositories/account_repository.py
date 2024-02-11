fd9572c08ae2288c4eb5e23c0132f5f242aa5075
chrishombach <chris.hombach@gmail.com>
2024-02-11 13:48:00 +0100

    95 depo value in account repository (#138)
    * Removed unneed depot tables
    
    * Setup DepotFactory
    
    * Setup Depot Factory that returns depot
    
    * Get account_value Case right
    
    * Include Depot fields
    
    * Add new fields
    
    * ADd fields to view
    
    * Fix test
    
    * Fix test
    
    ---------
    
    Co-authored-by: Christoph Hombach <Christoph.Hombach@2xideas.com>

--- montrek/account/repositories/account_repository.py
+++ montrek/account/repositories/account_repository.py
@@ -1,5 +1,9 @@
+from decimal import Decimal
+from django.db.models.fields import decimal
 from django.utils import timezone
 from django.db.models import (
+    DecimalField,
+    ExpressionWrapper,
     QuerySet,
     Sum,
     F,
@@ -11,6 +15,7 @@ from django.db.models import (
     Subquery,
     OuterRef,
 )
+from numpy import who
 from account.models import (
     AccountHub,
     AccountStaticSatellite,
@@ -91,26 +96,86 @@ class AccountRepository(MontrekRepository):
         )
 
     def _account_value(self):
-        return Case(
+        self.annotations["account_cash"] = self._get_bank_account_cash()
+        self.annotations["account_depot_value"] = Case(
+            When(
+                account_type=AccountStaticSatellite.AccountType.DEPOT,
+                then=self._get_depot_value(),
+            ),
+            default=Value(Decimal(0.0)),
+        )
+        self.annotations["account_depot_book_value"] = Case(
+            When(
+                account_type=AccountStaticSatellite.AccountType.DEPOT,
+                then=self._get_depot_book_value(),
+            ),
+            default=Value(Decimal(0.0)),
+        )
+        self.annotations["account_depot_pnl"] = Case(
+            When(
+                account_type=AccountStaticSatellite.AccountType.DEPOT,
+                then=self._get_depot_pnl(),
+            ),
+            default=Value(Decimal(0.0)),
+        )
+        self.annotations["account_depot_performance"] = Case(
+            When(
+                account_type=AccountStaticSatellite.AccountType.DEPOT,
+                then=self._get_depot_performance(),
+            ),
+            default=Value(Decimal(0.0)),
+        )
+        self.annotations["account_value"] = Case(
+            When(
+                account_type=AccountStaticSatellite.AccountType.BANK_ACCOUNT,
+                then=F("account_cash"),
+            ),
             When(
                 account_type=AccountStaticSatellite.AccountType.DEPOT,
                 then=self._get_depot_account_value(),
             ),
-            default=self._get_bank_account_value(),
+            When(
+                account_type=AccountStaticSatellite.AccountType.OTHER,
+                then=Value(Decimal(0.0)),
+            ),
         )
 
-    def _get_bank_account_value(self):
+    def _get_bank_account_cash(self):
         transaction_amount_sq = Subquery(
             self.transaction_table_subquery()
             .values("link_transaction_account")
             .annotate(account_value=Sum(F("transaction_value")))
             .values("account_value")
         )
-        self.annotations["account_value"] = transaction_amount_sq
+        return transaction_amount_sq
+
+    def _get_depot_value(self):
+        return self._get_depot_subquery("value")
+
+    def _get_depot_book_value(self):
+        return self._get_depot_subquery("book_value")
+
+    def _get_depot_pnl(self):
+        return self._get_depot_subquery("profit_loss")
+
+    def _get_depot_performance(self):
+        return ExpressionWrapper(
+            F("account_depot_pnl") / F("account_depot_book_value"),
+            output_field=DecimalField(),
+        )
 
     def _get_depot_account_value(self):
-        # TODO: Set to Depot Value
-        return self._get_bank_account_value()
+        return ExpressionWrapper(
+            F("account_cash") + F("account_depot_value"), output_field=DecimalField()
+        )
+
+    def _get_depot_subquery(self, field):
+        return Subquery(
+            self.get_depot_data(OuterRef("pk"))
+            .values("account_id")
+            .annotate(**{field: Sum(F(field))})
+            .values(field)
+        )
 
     @paginated_table
     def get_upload_registry_table_by_account_paginated(self, account_hub_id: int):
@@ -135,11 +200,10 @@ class AccountRepository(MontrekRepository):
         )
 
     def get_depot_data(self, account_hub_id: int):
-        hub_entity = self.hub_class.objects.get(pk=account_hub_id)
         return (
             DepotRepository(self.session_data)
             .std_queryset()
-            .filter(account_id=hub_entity.id)
+            .filter(account_id=account_hub_id)
         )
 
     @paginated_table
--- montrek/depot/tests/factories/depot_factories.py
+++ montrek/depot/tests/factories/depot_factories.py
@@ -1,11 +1,57 @@
 import factory
+from account.models import AccountStaticSatellite
+from currency.tests.factories.currency_factories import (
+    CurrencyTimeSeriesSatelliteFactory,
+)
+from transaction.tests.factories.transaction_factories import (
+    TransactionSatelliteFactory,
+)
+from asset.tests.factories.asset_factories import (
+    AssetStaticSatelliteFactory,
+    AssetTimeSeriesSatelliteFactory,
+)
+from account.tests.factories.account_factories import AccountStaticSatelliteFactory
 
-class DepotHubFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = 'depot.DepotHub'
 
-class DepotSatelitteFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = 'depot.DepotSatelitte'
-    hub_entity = factory.SubFactory(DepotHubFactory)
-    depot_name = factory.Sequence(lambda n: f'depot_name_{n}')
+class DepotAccountFactory(AccountStaticSatelliteFactory):
+    account_type = AccountStaticSatellite.AccountType.DEPOT
+
+    @factory.post_generation
+    def depot_data(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            # No mechanism to create depot data installed yet
+            return
+        else:
+            # Get 4 Euros cash
+            TransactionSatelliteFactory.create(
+                hub_entity__account=self.hub_entity,
+                transaction_amount=4,
+                transaction_price=1,
+                transaction_date="2020-12-31",
+            )
+            assets = AssetTimeSeriesSatelliteFactory.create_batch(
+                3, price=1.2, value_date="2021-01-01"
+            )
+            currency = CurrencyTimeSeriesSatelliteFactory.create(
+                fx_rate=1, value_date="2021-01-01"
+            )
+            for asset in assets:
+                AssetStaticSatelliteFactory.create(
+                    hub_entity=asset.hub_entity,
+                    currency=currency.hub_entity,
+                )
+                TransactionSatelliteFactory.create(
+                    hub_entity__asset=asset.hub_entity,
+                    hub_entity__account=self.hub_entity,
+                    transaction_amount=1,
+                    transaction_price=1,
+                    transaction_date="2021-01-01",
+                )
+                TransactionSatelliteFactory.create(
+                    hub_entity__account=self.hub_entity,
+                    transaction_amount=1,
+                    transaction_price=-2,
+                    transaction_date="2021-01-01",
+                )
