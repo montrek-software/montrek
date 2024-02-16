3a4ec94ecea11b2a705519d80226f0b6fc562e36
Vincent Mohiuddin <vincent.mohiuddin@2xideas.com>
2024-02-16 16:56:58 +0100

    add model tests

--- montrek/user/tests/test_models.py
+++ montrek/user/tests/test_models.py
@@ -1,14 +1,49 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from user.models import MontrekUser, MontrekUserManager
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from user import models as user_models
+from django.contrib.auth import models as auth_models
+
 
 class TestMontrekUser(TestCase):
     def test_is_active_auth_user_model(self):
-        self.assertEqual(get_user_model(), MontrekUser)
+        self.assertEqual(get_user_model(), user_models.MontrekUser)
 
     def test_is_abstract_base_user(self):
-        self.assertTrue(issubclass(MontrekUser, AbstractUser))
+        self.assertTrue(issubclass(user_models.MontrekUser, auth_models.AbstractUser))
 
     def test_manager_is_base_user_manager(self):
-        self.assertTrue(issubclass(MontrekUserManager, BaseUserManager))
+        self.assertTrue(
+            issubclass(user_models.MontrekUserManager, auth_models.BaseUserManager)
+        )
+
+    def test_create_user(self):
+        user = get_user_model().objects.create_user(
+            email="test@example.com", password="testpassword"
+        )
+        db_users = get_user_model().objects.all()
+
+        self.assertEqual(len(db_users), 1)
+        self.assertEqual(db_users[0], user)
+        self.assertFalse(user.is_staff)
+
+    def test_create_user_no_email(self):
+        with self.assertRaises(ValueError) as e:
+            get_user_model().objects.create_user(email="", password="testpassword")
+
+        self.assertEqual(str(e.exception), "User must have an email address.")
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            email="test@example.com", password="testpassword"
+        )
+        db_users = get_user_model().objects.all()
+
+        self.assertEqual(len(db_users), 1)
+        self.assertEqual(db_users[0], user)
+        self.assertTrue(user.is_staff)
+
+    def test_user_str(self):
+        user = get_user_model().objects.create_user(
+            email="test@example.com", password="testpassword"
+        )
+        self.assertEqual(str(user), "test@example.com")
