60faff2486546f587884048fcf448d38bf8779ad
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-02-18 20:26:28 +0100

    Setup class test

--- montrek/montrek_example/tests/test_views.py
+++ montrek/montrek_example/tests/test_views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from montrek_example import views
 from montrek_example.tests.factories import montrek_example_factories as me_factories
 from montrek_example.repositories.hub_a_repository import HubARepository
+from baseclasses.utils import montrek_time
 
 
 class TestMontrekExampleACreateView(TestCase):
@@ -35,7 +36,41 @@ class TestMontrekExampleACreateView(TestCase):
 class TestMontrekExampleAHistoryView(TestCase):
     def test_view_return_correct_html(self):
         sat_a = me_factories.SatA1Factory()
-        url = reverse("montrek_example_a_history", kwargs={"pk": sat_a.id})
+        url = reverse("montrek_example_a_history", kwargs={"pk": sat_a.hub_entity.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "montrek_table.html")
+
+    def test_view_with_history_data(self):
+        huba = me_factories.HubAFactory()
+        me_factories.SatA1Factory(
+            hub_entity=huba,
+            field_a1_str="TestFeld",
+            field_a1_int=5,
+            state_date_end=montrek_time(2024, 2, 17),
+        )
+        me_factories.SatA1Factory(
+            hub_entity=huba,
+            field_a1_str="TestFeld",
+            field_a1_int=6,
+            state_date_start=montrek_time(2024, 2, 17),
+        )
+        me_factories.SatA2Factory(
+            hub_entity=huba,
+            field_a2_str="ConstantTestFeld",
+            field_a2_float=6.0,
+        )
+        url = reverse("montrek_example_a_history", kwargs={"pk": huba.id})
+        response = self.client.get(url)
+        test_queryset = response.context_data["object_list"]
+        self.assertEqual(test_queryset.count(), 2)
+        self.assertEqual(test_queryset[0].field_a1_int, 5)
+        self.assertEqual(test_queryset[1].field_a1_int, 6)
+        self.assertEqual(test_queryset[0].change_date[:10], "0001-01-01")
+        self.assertEqual(test_queryset[1].change_date[:10], "2024-02-17")
+
+        self.assertEqual(test_queryset[0].field_a1_str, test_queryset[1].field_a1_str)
+        self.assertEqual(test_queryset[0].field_a2_str, test_queryset[1].field_a2_str)
+        self.assertEqual(
+            test_queryset[0].field_a2_float, test_queryset[1].field_a2_float
+        )
