20522a63c46b7a658fcd1c23683af8ac74c4c9f7
chrishombach <chris.hombach@gmail.com>
2024-02-21 21:56:57 +0100

    160 links in env (#161)
    * Add links via base view
    
    * read links from .env
    
    * Remove unused code
    
    ---------
    
    Co-authored-by: Christoph Hombach <Christoph.Hombach@2xideas.com>

---
+++ montrek/baseclasses/templatetags/base_tags.py
@@ -0,0 +1,19 @@
+from django import template
+from django.utils.safestring import mark_safe
+from baseclasses.views import navbar, links
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def include_navbar(context):
+    request = context["request"]
+    response = navbar(request)
+    return mark_safe(response.content.decode("utf-8"))
+
+
+@register.simple_tag(takes_context=True)
+def include_links(context):
+    request = context["request"]
+    response = links(request)
+    return mark_safe(response.content.decode("utf-8"))
--- montrek/baseclasses/views.py
+++ montrek/baseclasses/views.py
@@ -9,6 +9,7 @@ from django.urls import reverse
 from django.contrib import messages
 from decouple import config
 from baseclasses.dataclasses.nav_bar_model import NavBarModel
+from baseclasses.dataclasses.link_model import LinkModel
 from baseclasses.pages import NoPage
 from baseclasses.forms import DateRangeForm, FilterForm
 from baseclasses.forms import MontrekCreateForm
@@ -30,6 +31,15 @@ def navbar(request):
     navbar_apps_config = config("NAVBAR_APPS", default="").split(" ")
     navbar_apps = [NavBarModel(app) for app in navbar_apps_config if app != ""]
     return render(request, "navbar.html", {"nav_apps": navbar_apps})
+
+
+def links(request):
+    links_config = config("LINKS", default="http://example.com,Example").split(" ")
+    links = []
+    for link in links_config:
+        link_constituents = link.split(",")
+        links.append(LinkModel(href=link_constituents[0], title=link_constituents[1]))
+    return render(request, "links.html", {"links": links})
 
 
 class MontrekPageViewMixin:
