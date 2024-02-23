074117f177ab4d52ea759d5979ec34598539f3bf
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-02-23 06:34:47 +0100

    Cleanup

--- montrek/montrek/urls.py
+++ montrek/montrek/urls.py
@@ -26,17 +26,10 @@ urlpatterns = [
         "under_construction", base_views.under_construction, name="under_construction"
     ),
     path("admin/", admin.site.urls),
-    # path("account/", include("account.urls")),
+    path("user/", include("user.urls")),
     path("baseclasses/", include("baseclasses.urls")),
     path("file_upload/", include("file_upload.urls")),
-    # path("transaction/", include("transaction.urls")),
-    # path("asset/", include("asset.urls")),
-    # path("credit_institution/", include("credit_institution.urls")),
-    # path("currency/", include("currency.urls")),
-    # path("country/", include("country.urls")),
     path("montrek_example/", include("montrek_example.urls")),
-    path("user/", include("user.urls")),
-    # path("company/", include("company.urls")),
 ]
 
 urlpatterns += [
