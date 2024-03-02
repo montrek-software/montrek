59794b9feae01ba7a120ca056961934e94109b06
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-03-02 20:53:46 +0100

    Implements change

--- montrek/company/tests/repositories/test_company_respository.py
+++ montrek/company/tests/repositories/test_company_respository.py
@@ -46,9 +46,9 @@ class CompanyRepositoryTest(TestCase):
         self.assertEqual(companies[3].effectual_company_id, "TST")
         history_qs = repository.get_history_queryset(pk=companies[3].id)
         self.assertEqual(len(history_qs), 2)
-        self.assertEqual(history_qs[0].company_name, "TestCompany")
-        self.assertEqual(history_qs[1].company_name, "UnitedTestCompany")
-        self.assertEqual(history_qs[0].state_date_end, history_qs[1].state_date_start)
+        self.assertEqual(history_qs[1].company_name, "TestCompany")
+        self.assertEqual(history_qs[0].company_name, "UnitedTestCompany")
+        self.assertGreater(history_qs[0].change_date, history_qs[1].change_date)
 
     def test_get_all_time_series(self):
         for company in self.companies:
