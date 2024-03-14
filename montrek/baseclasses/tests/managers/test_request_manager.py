a05c9002c635f0d641405520e925e43ff5b93bd9
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-03-14 08:45:43 +0100

    Test json response

--- montrek/baseclasses/tests/managers/test_request_manager.py
+++ montrek/baseclasses/tests/managers/test_request_manager.py
@@ -33,3 +33,17 @@ class TestRequestManager(TestCase):
             manager.message,
             "401 Client Error: UNAUTHORIZED for url: https://httpbin.org/basic-auth/user/pass",
         )
+
+    def test_get_json_dummy(self):
+        manager = MockRequestManager()
+        response_json = manager.get_json("json")
+        self.assertEqual(manager.status_code, 200)
+        self.assertEqual(manager.message, "OK")
+        self.assertNotEqual(response_json, {})
+
+    def test_get_json_no_valid_json(self):
+        manager = MockRequestManagerNoAuth()
+        response_json = manager.get_json("html")
+        self.assertEqual(manager.status_code, 0)
+        self.assertEqual(manager.message, "No valid json returned")
+        self.assertEqual(response_json, {})
