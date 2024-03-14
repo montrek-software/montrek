22bc81af2cf77cde12c189821e7a4ef88349e078
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-03-14 06:39:34 +0100

    Sort out messaging

--- montrek/baseclasses/managers/request_manager.py
+++ montrek/baseclasses/managers/request_manager.py
@@ -16,11 +16,17 @@ class RequestManager:
         self.status_code = 0
         self.message = "No get request made"
 
-    def get_json(self, endpoint):
+    def get_json(self, endpoint: str) -> dict:
         endpoint_url = f"{self.base_url}{endpoint}"
         request = requests.get(
             endpoint_url, auth=(self.authenticator.user, self.authenticator.password)
         )
         self.status_code = request.status_code
-        self.message = request.text
-        return request.json()
+        if request.ok:
+            self.message = "OK"
+            return request.json()
+        try:
+            request.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            self.message = str(e)
+        return {}
--- montrek/baseclasses/tests/managers/test_request_manager.py
+++ montrek/baseclasses/tests/managers/test_request_manager.py
@@ -7,10 +7,24 @@ class MockRequestManager(RequestManager):
     authenticator = RequestAuthenticator(user="user", password="pass")
 
 
+class MockRequestManagerNoAuth(RequestManager):
+    base_url = "https://httpbin.org/"
+
+
 class TestRequestManager(TestCase):
     def test_get_json(self):
         manager = MockRequestManager()
         response_json = manager.get_json("basic-auth/user/pass")
         self.assertEqual(manager.status_code, 200)
+        self.assertEqual(manager.message, "OK")
         self.assertEqual(response_json["authenticated"], True)
         self.assertEqual(response_json["user"], "user")
+
+    def test_get_json_unauthorized(self):
+        manager = MockRequestManagerNoAuth()
+        response_json = manager.get_json("basic-auth/user/pass")
+        self.assertEqual(manager.status_code, 401)
+        self.assertEqual(
+            manager.message,
+            "401 Client Error: UNAUTHORIZED for url: https://httpbin.org/basic-auth/user/pass",
+        )
