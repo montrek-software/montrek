adeac4a898143b824b65bc11bec39c1729d0d757
chrishombach <chris.hombach@gmail.com>
2024-03-18 06:09:52 +0000

    213 requestmanager (#214)
    * Base request manager
    
    * First implementation and test
    
    * Introduce authenticator
    
    * Sort out messaging
    
    * Include headers
    
    * Test json response
    
    * Include RequestBearerAuthenticator
    
    * Except no valid json response
    
    * Add Slug Authenticator
    
    ---------
    
    Co-authored-by: Christoph Hombach <Christoph.Hombach@2xideas.com>

---
+++ montrek/baseclasses/managers/request_manager.py
@@ -0,0 +1,62 @@
+import requests
+from base64 import b64encode
+
+
+class RequestAuthenticator:
+    def get_headers(self):
+        return {}
+
+
+class RequestUserPasswordAuthenticator:
+    def __init__(self, user: str, password: str):
+        self.user = user
+        self.password = password
+
+    def get_headers(self):
+        credentials = b64encode(f"{self.user}:{self.password}".encode()).decode()
+        return {"Authorization": f"Basic {credentials}"}
+
+
+class RequestBearerAuthenticator:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+        return {"Authorization": f"Bearer {self.token}"}
+
+
+class RequestSlugAuthenticator:
+    def __init__(self, slug: str, token: str):
+        self.slug = slug
+        self.token = token
+
+    def get_headers(self):
+        return {"Authorization": f"{self.slug}:{self.token}"}
+
+
+class RequestManager:
+    base_url = "NONESET"
+    authenticator = RequestAuthenticator()
+
+    def __init__(self):
+        self.status_code = 0
+        self.message = "No get request made"
+
+    def get_json(self, endpoint: str) -> dict:
+        endpoint_url = f"{self.base_url}{endpoint}"
+        request = requests.get(endpoint_url, headers=self.authenticator.get_headers())
+        self.status_code = request.status_code
+        if request.ok:
+            try:
+                json_response = request.json()
+            except requests.exceptions.JSONDecodeError:
+                self.message = "No valid json returned"
+                self.status_code = 0
+                return {}
+            self.message = "OK"
+            return json_response
+        try:
+            request.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            self.message = str(e)
+        return {}
---
+++ montrek/baseclasses/tests/managers/test_request_manager.py
@@ -0,0 +1,91 @@
+from django.test import TestCase
+from baseclasses.managers.request_manager import (
+    RequestManager,
+    RequestSlugAuthenticator,
+    RequestUserPasswordAuthenticator,
+    RequestBearerAuthenticator,
+)
+
+
+class MockRequestManager(RequestManager):
+    base_url = "https://httpbin.org/"
+    authenticator = RequestUserPasswordAuthenticator(user="user", password="pass")
+
+
+class MockRequestManagerNoAuth(RequestManager):
+    base_url = "https://httpbin.org/"
+    authenticator = RequestUserPasswordAuthenticator(user="user", password="wrongpass")
+
+
+class MockRequestManagerToken(RequestManager):
+    base_url = "https://httpbin.org/"
+    authenticator = RequestBearerAuthenticator(token="testtoken123")
+
+
+class TestRequestManager(TestCase):
+    def test_get_json(self):
+        manager = MockRequestManager()
+        response_json = manager.get_json("basic-auth/user/pass")
+        self.assertEqual(manager.status_code, 200)
+        self.assertEqual(manager.message, "OK")
+        self.assertEqual(response_json["authenticated"], True)
+        self.assertEqual(response_json["user"], "user")
+
+    def test_get_json_unauthorized(self):
+        manager = MockRequestManagerNoAuth()
+        response_json = manager.get_json("basic-auth/user/pass")
+        self.assertEqual(manager.status_code, 401)
+        self.assertEqual(response_json, {})
+        self.assertEqual(
+            manager.message,
+            "401 Client Error: UNAUTHORIZED for url: https://httpbin.org/basic-auth/user/pass",
+        )
+
+    def test_get_json_dummy(self):
+        manager = MockRequestManager()
+        response_json = manager.get_json("json")
+        self.assertEqual(manager.status_code, 200)
+        self.assertEqual(manager.message, "OK")
+        self.assertNotEqual(response_json, {})
+
+    def test_get_json_no_valid_json(self):
+        manager = MockRequestManagerNoAuth()
+        response_json = manager.get_json("html")
+        self.assertEqual(manager.status_code, 0)
+        self.assertEqual(manager.message, "No valid json returned")
+        self.assertEqual(response_json, {})
+
+    def test_bearer_authenticator(self):
+        manager = MockRequestManagerToken()
+        response_json = manager.get_json("bearer")
+        self.assertEqual(manager.status_code, 200)
+        self.assertEqual(manager.message, "OK")
+        self.assertEqual(response_json["authenticated"], True)
+        self.assertEqual(response_json["token"], "testtoken123")
+
+    def test_wrong_authenticator(self):
+        manager = MockRequestManagerToken()
+        response_json = manager.get_json("basic-auth/user/pass")
+        self.assertEqual(manager.status_code, 401)
+        self.assertEqual(response_json, {})
+        self.assertEqual(
+            manager.message,
+            "401 Client Error: UNAUTHORIZED for url: https://httpbin.org/basic-auth/user/pass",
+        )
+
+
+class TestRequestAuthenticators(TestCase):
+    def test_user_password_authenticator(self):
+        authenticator = RequestUserPasswordAuthenticator(user="user", password="pass")
+        headers = authenticator.get_headers()
+        self.assertEqual(headers["Authorization"], "Basic dXNlcjpwYXNz")
+
+    def test_bearer_authenticator(self):
+        authenticator = RequestBearerAuthenticator(token="testtoken123")
+        headers = authenticator.get_headers()
+        self.assertEqual(headers["Authorization"], "Bearer testtoken123")
+
+    def test_slug_authenticator(self):
+        authenticator = RequestSlugAuthenticator(slug="testslug", token="12456")
+        headers = authenticator.get_headers()
+        self.assertEqual(headers["Authorization"], "testslug:12456")
