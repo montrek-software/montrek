9895d4b1a53b484e0596c37ea575d7022028ec5b
Vincent Mohiuddin <vincent.mohiuddin@2xideas.com>
2024-03-29 10:36:20 +0100

    add factories

---
+++ montrek/file_upload/repositories/field_map_repository.py
@@ -0,0 +1,14 @@
+from baseclasses.repositories.montrek_repository import MontrekRepository
+from file_upload.models import FieldMapHub, FieldMapStaticSatellite
+
+
+class FieldMapRepository(MontrekRepository):
+    hub_class = FieldMapHub
+
+    def std_queryset(self, **kwargs):
+        self.add_satellite_fields_annotations(
+            FieldMapStaticSatellite,
+            ["source_field", "database_field"],
+        )
+        queryset = self.build_queryset()
+        return queryset
