2fa06dd51c7ab0b39a0756d6cda23be46fddaeb8
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-04-12 14:55:23 +0200

    Add table elements

--- montrek/mailing/views.py
+++ montrek/mailing/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from baseclasses.views import MontrekListView
+from baseclasses.dataclasses import table_elements as te
 from mailing.managers.mailing_manager import MailingManager
 from mailing.pages import MailingPage
 
@@ -12,3 +13,11 @@ class MailOverviewListView(MontrekListView):
     page_class = MailingPage
     title = "Mail Overview"
     tab = "tab_overview"
+
+    @property
+    def elements(self) -> list[te.TableElement]:
+        return [
+            te.StringTableElement(name="Subject", attr="mail_subject"),
+            te.StringTableElement(name="Recipients", attr="mail_recipients"),
+            te.StringTableElement(name="State", attr="mail_state"),
+        ]
