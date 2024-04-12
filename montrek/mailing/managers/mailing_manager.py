ae2ec6b965e95d38ab831774cf400ebe95789c7d
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-04-12 17:55:44 +0200

    Add messages (No effect right now)

--- montrek/mailing/managers/mailing_manager.py
+++ montrek/mailing/managers/mailing_manager.py
@@ -2,6 +2,10 @@ from django.conf import settings
 from django.core.mail import send_mail
 from baseclasses.managers.montrek_manager import MontrekManager
 from mailing.repositories.mailing_repository import MailingRepository
+from baseclasses.dataclasses.montrek_message import (
+    MontrekMessageInfo,
+    MontrekMessageError,
+)
 from smtplib import SMTPException
 
 
@@ -30,6 +34,9 @@ class MailingManager(MontrekManager):
                 "hub_entity_id": mail_hub.id,
             }
             self.repository.std_create_object(mail_success_params)
+            self.messages.append(
+                MontrekMessageInfo(message=f"Mail successfully sent to {recipients}")
+            )
         except SMTPException as e:
             mail_fail_params = {
                 "mail_state": "Failed",
@@ -37,3 +44,6 @@ class MailingManager(MontrekManager):
                 "hub_entity_id": mail_hub.id,
             }
             self.repository.std_create_object(mail_fail_params)
+            self.messages.append(
+                MontrekMessageError(message=f"Mail failed to send to {recipients}")
+            )
