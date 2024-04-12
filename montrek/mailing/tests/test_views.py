a0e9339fd098b002e00c390b5ddbe952ea230226
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-04-12 15:50:48 +0200

    Test Send Mail form

--- montrek/mailing/tests/test_views.py
+++ montrek/mailing/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from mailing.tests.factories.mailing_factories import MailSatelliteFactory
 from mailing import views
+from mailing.forms import MailingSendForm
+from user.tests.factories.montrek_user_factories import MontrekUserFactory
 
 
 class TestMailsOverview(TestCase):
@@ -32,13 +34,42 @@ class TestMailsOverview(TestCase):
         self.assertIsInstance(context["view"], views.MailOverviewListView)
 
 
+class MockManager:
+    def __init__(self, session_data: dict):
+        pass
+
+    def send_mail(self, data: dict):
+        pass
+
+
+class MockSendMailView(views.SendMailView):
+    manager_class = MockManager
+
+
 class TestSendMailView(TestCase):
+    def setUp(self) -> None:
+        self.user = MontrekUserFactory()
+        self.client.force_login(self.user)
+
     def test_view_page(self):
         view = views.SendMailView()
         view.kwargs = {}
         page_context = view.get_page_context({})
         self.assertNotEqual(page_context["page_title"], "page_title not set!")
         self.assertNotEqual(page_context["title"], "No Title set!")
+
+    def test_send_mail(self):
+        class MockForm:
+            cleaned_data: dict = {}
+
+        view = MockSendMailView()
+        mock_form = MockForm()
+        response = view.form_valid(mock_form)
+        self.assertEqual(response.url, "/mailing/overview")
+
+    def test_view_form(self):
+        view = views.SendMailView()
+        self.assertEqual(view.form_class, MailingSendForm)
 
     def test_account_overview_returns_correct_html(self):
         response = self.client.get("/mailing/send")
--- montrek/mailing/views.py
+++ montrek/mailing/views.py
@@ -1,9 +1,13 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-from baseclasses.views import MontrekListView, MontrekCreateView
+from baseclasses.views import MontrekCreateUpdateView, MontrekListView
 from baseclasses.dataclasses import table_elements as te
 from mailing.managers.mailing_manager import MailingManager
 from mailing.pages import MailingPage
+from baseclasses.dataclasses.view_classes import ActionElement
+from mailing.forms import MailingSendForm
 
 
 # Create your views here.
@@ -22,9 +26,30 @@ class MailOverviewListView(MontrekListView):
             te.StringTableElement(name="State", attr="mail_state"),
         ]
 
+    @property
+    def actions(self) -> tuple[ActionElement]:
+        action_send_mail = ActionElement(
+            icon="envelope",
+            link=reverse("send_mail"),
+            action_id="id_send_mail",
+            hover_text="Send Mail",
+        )
+        return (action_send_mail,)
 
-class SendMailView(MontrekCreateView):
+
+class SendMailView(MontrekCreateUpdateView):
     manager_class = MailingManager
     page_class = MailingPage
     title = "Send mail"
     tab = "tab_send_mail"
+    form_class = MailingSendForm
+    success_url = "mailing"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tag"] = "Send Mail"
+        return context
+
+    def form_valid(self, form):
+        self.manager.send_mail(form.cleaned_data)
+        return HttpResponseRedirect(self.get_success_url())
