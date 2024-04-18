7b3c99419bab2d80d4be3eeef54efeed9bfb76f3
chrishombach <chris.hombach@gmail.com>
2024-04-18 22:11:35 +0200

    255 mail template (#257)
    * Add template
    
    * Include template
    
    * Modify template
    
    * Remove imports
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Christoph Hombach <Christoph.Hombach@2xideas.com>

--- montrek/mailing/managers/mailing_manager.py
+++ montrek/mailing/managers/mailing_manager.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 from baseclasses.managers.montrek_manager import MontrekManager
 from mailing.repositories.mailing_repository import MailingRepository
 from baseclasses.dataclasses.montrek_message import (
@@ -12,7 +13,13 @@ from smtplib import SMTPException
 class MailingManager(MontrekManager):
     repository_class = MailingRepository
 
-    def send_montrek_mail(self, recipients: str, subject: str, message: str):
+    @property
+    def template_path(self):
+        return settings.EMAIL_TEMPLATE
+
+    def send_montrek_mail(
+        self, recipients: str, subject: str, message: str, additional_parms: dict = {}
+    ):
         recipient_list = recipients.replace(" ", "").split(",")
         mail_params: dict = {
             "mail_subject": subject,
@@ -21,13 +28,17 @@ class MailingManager(MontrekManager):
             "mail_state": "Pending",
         }
         mail_hub = self.repository.std_create_object(mail_params)
+        body = self.get_mail_body(message, additional_parms)
+
         try:
-            send_mail(
-                subject=subject,
-                message=message,
-                recipient_list=recipient_list,
-                from_email=settings.EMAIL_BACKEND,
+            email = EmailMessage(
+                subject,
+                body,
+                settings.EMAIL_BACKEND,
+                recipient_list,
             )
+            email.content_subtype = "html"
+            email.send()
             mail_success_params = {
                 "mail_state": "Sent",
                 "mail_comment": "Successfully send",
@@ -47,3 +58,15 @@ class MailingManager(MontrekManager):
             self.messages.append(
                 MontrekMessageError(message=f"Mail failed to send to {recipients}")
             )
+
+    def get_mail_body(
+        self,
+        message: str,
+        additional_parms: dict,
+    ):
+        return render_to_string(
+            self.template_path, self.get_context_data(message, additional_parms)
+        )
+
+    def get_context_data(self, message: str, additional_parms: dict):
+        return {"message": message, **additional_parms}
