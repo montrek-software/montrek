4bc33326a07b326d2555c344eb8cc9eae787fac8
chrishombach <chris.hombach@gmail.com>
2024-04-26 14:32:21 +0200

    272 all views access montrektablemanager (#275)
    * Fix country
    
    * First implement
    
    * Move table elements montrek_example
    
    * ove tests
    
    * Set stylings
    
    * Set stylings
    
    * Fix tests
    
    * Move csv export
    
    * Remove ol list manager
    
    * Fix test
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Christoph Hombach <Christoph.Hombach@2xideas.com>

--- montrek/montrek_example/managers/montrek_example_managers.py
+++ montrek/montrek_example/managers/montrek_example_managers.py
@@ -1,21 +1,88 @@
-from baseclasses.managers.montrek_manager import MontrekManager
+from reporting.managers.montrek_table_manager import MontrekTableManager
+from reporting.dataclasses import table_elements as te
 from montrek_example.repositories.hub_a_repository import HubARepository
 from montrek_example.repositories.hub_b_repository import HubBRepository
 from montrek_example.repositories.hub_c_repository import HubCRepository
 from montrek_example.repositories.hub_d_repository import HubDRepository
 
 
-class HubAManager(MontrekManager):
+class HubAManager(MontrekTableManager):
     repository_class = HubARepository
 
+    @property
+    def table_elements(self) -> list:
+        return (
+            te.StringTableElement(name="A1 String", attr="field_a1_str"),
+            te.IntTableElement(name="A1 Int", attr="field_a1_int"),
+            te.StringTableElement(name="A2 String", attr="field_a2_str"),
+            te.FloatTableElement(name="A2 Float", attr="field_a2_float"),
+            te.StringTableElement(name="B1 String", attr="field_b1_str"),
+            te.LinkTableElement(
+                name="View",
+                url="montrek_example_a_details",
+                kwargs={"pk": "id"},
+                icon="eye-open",
+                hover_text="View Example A",
+            ),
+            te.LinkTableElement(
+                name="Update",
+                url="montrek_example_a_update",
+                kwargs={"pk": "id"},
+                icon="pencil",
+                hover_text="View Example A",
+            ),
+            te.LinkTableElement(
+                name="Delete",
+                url="montrek_example_a_delete",
+                kwargs={"pk": "id"},
+                icon="trash",
+                hover_text="Delete Example A",
+            ),
+        )
 
-class HubBManager(MontrekManager):
+
+class HubBManager(MontrekTableManager):
     repository_class = HubBRepository
 
+    @property
+    def table_elements(self) -> list:
+        return [
+            te.StringTableElement(name="B1 String", attr="field_b1_str"),
+            te.IntTableElement(name="B1 Date", attr="field_b1_date"),
+            te.StringTableElement(name="B2 String", attr="field_b2_str"),
+            te.StringTableElement(name="B2 Choice", attr="field_b2_choice"),
+            te.LinkTextTableElement(
+                name="D2 String",
+                text="field_d1_str",
+                url="montrek_example_d_list",
+                hover_text="View D Example",
+                kwargs={"filter": "field_d1_str"},
+            ),
+            te.StringTableElement(name="D2 Int", attr="field_d1_int"),
+            te.AlertTableElement(name="Alert Level", attr="alert_level"),
+            te.StringTableElement(name="Alert Message", attr="alert_message"),
+        ]
 
-class HubCManager(MontrekManager):
+
+class HubCManager(MontrekTableManager):
     repository_class = HubCRepository
 
+    @property
+    def table_elements(self) -> list:
+        return (
+            te.StringTableElement(name="B1 String", attr="field_b1_str"),
+            te.IntTableElement(name="B1 Date", attr="field_b1_date"),
+            te.StringTableElement(name="B2 String", attr="field_b2_str"),
+            te.StringTableElement(name="B2 Choice", attr="field_b2_choice"),
+        )
 
-class HubDManager(MontrekManager):
+
+class HubDManager(MontrekTableManager):
     repository_class = HubDRepository
+
+    @property
+    def table_elements(self) -> list:
+        return [
+            te.StringTableElement(name="D1 String", attr="field_d1_str"),
+            te.IntTableElement(name="D1 Int", attr="field_d1_int"),
+        ]
--- montrek/reporting/tests/managers/test_montrek_table_manager.py
+++ montrek/reporting/tests/managers/test_montrek_table_manager.py
@@ -12,12 +12,36 @@ class MockData:
     field_c: float
 
 
+class MockQuerySet:
+    def __init__(self, *args):
+        self.items = args
+
+    def __iter__(self):
+        return iter(self.items)
+
+    def __len__(self):
+        return len(self.items)
+
+    def __getitem__(self, index):
+        return self.items[index]
+
+    def __eq__(self, other):
+        if isinstance(other, list):
+            return list(self.items) == other
+        return NotImplemented
+
+    def all(self):
+        return self.items
+
+
 class MockRepository:
     def __init__(self, session_data: dict):
         self.session_data = session_data
 
     def std_queryset(self):
-        return [MockData("a", 1, 1.0), MockData("b", 2, 2.0), MockData("c", 3, 3.0)]
+        return MockQuerySet(
+            MockData("a", 1, 1.0), MockData("b", 2, 2.0), MockData("c", 3, 3.0)
+        )
 
 
 class MockMontrekTableManager(MontrekTableManager):
@@ -48,6 +72,16 @@ class MockMontrekTableManager(MontrekTableManager):
         )
 
 
+class MockHttpResponse:
+    content: str = ""
+
+    def write(self, content):
+        self.content += content
+
+    def getvalue(self):
+        return self.content
+
+
 class TestMontrekTableManager(TestCase):
     def test_to_html(self):
         test_html = MockMontrekTableManager().to_html()
@@ -59,3 +93,11 @@ class TestMontrekTableManager(TestCase):
         expected_headers = ["Field A", "Field B", "Field C", "Link", "Link Text"]
         header_texts = [th.get_text() for th in headers]
         self.assertEqual(header_texts, expected_headers)
+
+    def test_download_csv(self):
+        manager = MockMontrekTableManager()
+        response = manager.download_csv(MockHttpResponse())
+        self.assertEqual(
+            response.getvalue(),
+            "Field A,Field B,Field C,Link Text\r\na,1,1.0,a\r\nb,2,2.0,b\r\nc,3,3.0,c\r\n",
+        )
