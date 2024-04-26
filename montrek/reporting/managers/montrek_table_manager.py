0016460e2c35a8d926ea8d2a64a191dceccb878e
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-04-26 13:24:25 +0200

    Set stylings

--- montrek/reporting/managers/montrek_table_manager.py
+++ montrek/reporting/managers/montrek_table_manager.py
@@ -8,7 +8,7 @@ class MontrekTableManager(MontrekManager):
         return ()
 
     def to_html(self):
-        html_str = '<table class="table table-borded table-hover"><tr>'
+        html_str = '<table class="table table-bordered table-hover"><tr>'
         for table_element in self.table_elements:
             html_str += f"<th>{table_element.name}</th>"
         html_str += "</tr>"
