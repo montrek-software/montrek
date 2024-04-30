49e3fa361dca0adc05631becf2adf3fc33e286a9
Vincent Mohiuddin <vincent.mohiuddin@2xideas.com>
2024-04-30 08:42:37 +0200

    middleware draft

---
+++ montrek/middleware/permission_error_middleware.py
@@ -0,0 +1,18 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+
+
+class PermissionErrorMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_exception(self, request, exception):
+        from django.core.exceptions import PermissionDenied
+
+        if isinstance(exception, PermissionDenied):
+            # Redirect to the login page
+            return redirect(reverse("login"))
