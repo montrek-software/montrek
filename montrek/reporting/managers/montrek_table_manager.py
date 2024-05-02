5559f443dbe8e263172c79ad61cbea359207158c
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-05-02 10:12:08 +0200

    Restore file

---
+++ montrek/reporting/managers/montrek_table_manager.py
@@ -0,0 +1,92 @@
+from django.views.generic.base import HttpResponse
+from django.core.paginator import Paginator
+from baseclasses.managers.montrek_manager import MontrekManager
+from reporting.dataclasses import table_elements as te
+import csv
+
+
+class MontrekTableManager(MontrekManager):
+    is_paginated = True
+    paginate_by = 10
+
+    @property
+    def table_elements(self) -> tuple[te.TableElement, ...]:
+        return ()
+
+    def to_html(self):
+        html_str = '<table class="table table-bordered table-hover"><tr>'
+        for table_element in self.table_elements:
+            html_str += f"<th>{table_element.name}</th>"
+        html_str += "</tr>"
+        queryset = self.get_paginated_queryset()
+        for query_object in queryset:
+            html_str += '<tr style="white-space:nowrap;">'
+            for table_element in self.table_elements:
+                html_str += table_element.get_attribute(query_object, "html")
+            html_str += "</tr>"
+        html_str += "</table>"
+        return html_str
+
+    def to_latex(self):
+        table_start_str = "\\begin{table}\n\\centering\n\\arrayrulecolor{lightgrey}\n\\begin{tabularx}{\\textwidth}{|"
+        table_end_str = "\\end{tabularx}\n\\end{table}"
+
+        column_def_str = ""
+        column_header_str = "\\rowcolor{blue}"
+
+        for table_element in self.table_elements:
+            if isinstance(table_element, te.LinkTableElement):
+                continue
+            column_def_str += "X|"
+            column_header_str += f"\\color{{white}}\\textbf{{{table_element.name}}} & "
+        table_start_str += column_def_str
+        table_start_str += "}\n\\hline\n"
+        table_start_str += column_header_str[:-2] + "\\\\\n\\hline\n"
+        latex_str = table_start_str
+
+        queryset = self.repository.std_queryset()
+        for i, query_object in enumerate(queryset):
+            if i % 2 == 0:
+                latex_str += "\\rowcolor{lightblue}"
+            for table_element in self.table_elements:
+                if isinstance(table_element, te.LinkTableElement):
+                    continue
+                latex_str += table_element.get_attribute(query_object, "latex")
+            latex_str = latex_str[:-2] + "\\\\\n\\hline\n"
+            if (i + 1) % 30 == 0:
+                latex_str += table_end_str
+                latex_str += table_start_str
+        latex_str += table_end_str
+        return latex_str
+
+    def get_paginated_queryset(self):
+        queryset = self.repository.std_queryset()
+        if self.is_paginated:
+            return self._paginate_queryset(queryset)
+        return queryset
+
+    def _paginate_queryset(self, queryset):
+        page_number = self.session_data.get("page", [1])[0]
+        paginator = Paginator(queryset, self.paginate_by)
+        page = paginator.get_page(page_number)
+        return page
+
+    def download_csv(self, response: HttpResponse) -> HttpResponse:
+        queryset = self.repository.std_queryset()
+        writer = csv.writer(response)
+        csv_elements = [
+            table_element
+            for table_element in self.table_elements
+            if not isinstance(table_element, te.LinkTableElement)
+        ]
+        writer.writerow([table_element.name for table_element in csv_elements])
+        for row in queryset.all():
+            values = []
+            for element in csv_elements:
+                if hasattr(element, "attr"):
+                    values.append(getattr(row, element.attr))
+                else:
+                    values.append(getattr(row, element.text))
+
+            writer.writerow(values)
+        return response
