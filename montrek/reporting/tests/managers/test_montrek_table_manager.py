a306c3465cb9302ace07e192ec96d41952cebad3
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-05-16 08:06:03 +0200

    Fix test

--- montrek/reporting/tests/managers/test_montrek_table_manager.py
+++ montrek/reporting/tests/managers/test_montrek_table_manager.py
@@ -96,7 +96,7 @@ class TestMontrekTableManager(TestCase):
 
     def test_to_latex(self):
         test_latex = MockMontrekTableManager().to_latex()
-        self.assertTrue(test_latex.startswith("\\begin{table}"))
+        self.assertTrue(test_latex.startswith("\n\\begin{table}"))
         self.assertTrue(test_latex.endswith("\\end{table}\n\n"))
 
     def test_download_csv(self):
