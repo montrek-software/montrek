6ebb6a535bb7d188da6991973022e458edbb2706
Vincent Mohiuddin <vincent.mohiuddin@2xideas.com>
2024-05-23 07:29:25 +0200

    draft

--- montrek/middleware/__init__.py
+++ montrek/middleware/__init__.py
@@ -1,3 +1,4 @@
 from middleware.permission_error_middleware import PermissionErrorMiddleware
+from middleware.login_required_middleware import LoginRequiredMiddleware
 
-__all__ = ["PermissionErrorMiddleware"]
+__all__ = ["PermissionErrorMiddleware", "LoginRequiredMiddleware"]
