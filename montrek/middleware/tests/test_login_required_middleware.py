29c08c957baad21b25b4d846cb2418da9700f70c
Vincent Mohiuddin <vincent.mohiuddin@2xideas.com>
2024-05-23 08:24:41 +0200

    add tests

---
+++ montrek/middleware/tests/test_login_required_middleware.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.urls import reverse
+from user.tests.factories.montrek_user_factories import MontrekUserFactory
+
+
+class TestLoginRequiredMiddleware(TestCase):
+    def test_middleware_redirects_to_login_page(self):
+        response = self.client.get("/")
+        self.assertRedirects(response, reverse("login"))
+
+    def test_middleware_does_not_redirect_for_authenticated_user(self):
+        user = MontrekUserFactory()
+        self.client.force_login(user)
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_middleware_does_not_redirect_for_exempt_path(self):
+        response = self.client.get(reverse("password_reset"))
+        self.assertEqual(response.status_code, 200)
