c2240a387eef2ca7e4b1aecfc37cec465c93c1ab
Vincent Mohiuddin <vincent.mohiuddin@2xideas.com>
2024-05-23 08:42:44 +0200

    define redirect once

--- montrek/middleware/login_required_middleware.py
+++ montrek/middleware/login_required_middleware.py
@@ -6,10 +6,11 @@ from django.conf import settings
 class LoginRequiredMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.login_redirect = HttpResponseRedirect(reverse_lazy("login"))
 
     def __call__(self, request):
         is_login_exempt = request.path.startswith(tuple(settings.LOGIN_EXEMPT_PATHS))
         if not request.user.is_authenticated and not is_login_exempt:
-            return HttpResponseRedirect(reverse_lazy("login"))
+            return self.login_redirect
         response = self.get_response(request)
         return response
