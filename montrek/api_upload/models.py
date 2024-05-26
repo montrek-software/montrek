a446bbc482331b3340d5fe5a1399d7efce986644
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-05-26 21:19:09 +0200

    Set API ABC

--- montrek/api_upload/models.py
+++ montrek/api_upload/models.py
@@ -3,11 +3,15 @@ from django.db import models
 from baseclasses import models as baseclass_models
 
 
-class ApiUploadRegistryHub(baseclass_models.MontrekHubABC):
-    pass
+class ApiUploadRegistryHubABC(baseclass_models.MontrekHubABC):
+    class Meta:
+        abstract = True
 
 
-class ApiUploadRegistryStaticSatellite(baseclass_models.MontrekSatelliteABC):
+class ApiUploadRegistryStaticSatelliteABC(baseclass_models.MontrekSatelliteABC):
+    class Meta:
+        abstract = True
+
     class UploadStatus(models.TextChoices):
         PENDING = "pending"
         UPLOADED = "uploaded"
@@ -15,10 +19,18 @@ class ApiUploadRegistryStaticSatellite(baseclass_models.MontrekSatelliteABC):
         PROCESSED = "processed"
         FAILED = "failed"
 
-    hub_entity = models.ForeignKey(ApiUploadRegistryHub, on_delete=models.CASCADE)
+    hub_entity = models.ForeignKey(ApiUploadRegistryHubABC, on_delete=models.CASCADE)
     identifier_fields = ["hub_entity_id"]
     url = models.CharField(max_length=255)
     upload_status = models.CharField(
         max_length=20, choices=UploadStatus.choices, default=UploadStatus.PENDING
     )
     upload_message = models.TextField(default="")
+
+
+class ApiUploadRegistryHub(ApiUploadRegistryHubABC):
+    pass
+
+
+class ApiUploadRegistryStaticSatellite(ApiUploadRegistryStaticSatelliteABC):
+    hub_entity = models.ForeignKey(ApiUploadRegistryHub, on_delete=models.CASCADE)
