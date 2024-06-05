6e7b2d025146173c283cc2c85bb12991161e5423
chrishombach <chris.hombach@gmail.com>
2024-06-05 08:36:31 +0200

    Add fields and adjust file upload form (#342)
    * Add fields and adjust file upload form
    
    * Remove unused code
    
    ---------
    
    Co-authored-by: Christoph Hombach <Christoph.Hombach@2xideas.com>

--- montrek/file_upload/forms.py
+++ montrek/file_upload/forms.py
@@ -34,8 +34,8 @@ class FieldMapCreateForm(MontrekCreateForm):
         self.initial["function_name"] = "no_change"
 
     def _get_database_field_choices(self):
-        repository = self.related_manager.repository
-        return sorted(list(set([f.name for f in repository.std_satellite_fields()])))
+        field_names = self.related_manager.get_std_queryset_field_choices()
+        return [field_name[0] for field_name in field_names]
 
     def _get_function_name_choices(self):
         function_names = []
