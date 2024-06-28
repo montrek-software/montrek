2f178249f1b82c9e1e8bef185b22b4b1f3a396dc
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-06-28 16:02:11 +0200

    Remove old test settings

--- montrek/montrek/test_settings.py
+++
@@ -1,7 +0,0 @@
-from .settings import *  # Import base settings from settings.py
-
-# Override any settings for testing purposes
-
-EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
-EMAIL_TEMPLATE = "mail_templates/montrek_mail_template.html"
-MEDIA_ROOT = "Blummi"
