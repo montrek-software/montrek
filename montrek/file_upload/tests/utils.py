6b4783619020dda6fc02064f7a7da97899d8271b
chrishombach <chris.hombach@gmail.com>
2024-07-18 13:29:39 +0200

    General log excel file asserts (#405)
    * Fix navbar height
    
    * Make only table scrollable
    
    * FileLogTEsts
    
    * Write tests
    
    * Add util file
    
    ---------
    
    Co-authored-by: Christoph Hombach <Christoph.Hombach@2xideas.com>

---
+++ montrek/file_upload/tests/utils.py
@@ -0,0 +1,42 @@
+from baseclasses.repositories.montrek_repository import MontrekRepository
+import pandas as pd
+import os
+from django.conf import settings
+
+
+class LogFileTestMixin:
+    def assert_log_excel_file(
+        self,
+        registry_repository: MontrekRepository,
+        err_msg: str,
+        *,
+        additional_data: pd.DataFrame = None,
+        _startswith: bool = False,
+    ):
+        upload_registry_query = registry_repository.std_queryset()
+        self.assertEqual(upload_registry_query.count(), 1)
+        log_excel_file = upload_registry_query.first().log_file
+        log_excel_path = os.path.join(settings.MEDIA_ROOT, log_excel_file)
+        test_excel_df = pd.read_excel(log_excel_path)
+        result_msg = test_excel_df.set_index("Param").loc[
+            "Upload Message", "Log Meta Data"
+        ]
+        if _startswith:
+            self.assertTrue(result_msg.startswith(err_msg))
+        else:
+            self.assertEqual(
+                result_msg,
+                err_msg,
+            )
+        if additional_data is not None:
+            result_aditional_data = pd.read_excel(
+                log_excel_path, sheet_name="additional_data"
+            ).fillna("")
+            result_aditional_data = result_aditional_data.astype(str)
+            additional_data = additional_data.astype(str)
+
+            pd.testing.assert_frame_equal(
+                result_aditional_data.reset_index(drop=True),
+                additional_data.reset_index(drop=True),
+                check_dtype=False,
+            )
