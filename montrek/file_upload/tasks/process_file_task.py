b819b0e5bb0c4c2238f1f59d4d346d86909ff648
Vincent Mohiuddin <vincent.mohiuddin@2xideas.com>
2024-07-19 11:54:06 +0200

    test task

--- montrek/file_upload/tasks/process_file_task.py
+++ montrek/file_upload/tasks/process_file_task.py
@@ -9,14 +9,24 @@ from baseclasses.managers.montrek_manager import MontrekManager
 from file_upload.managers.file_upload_manager import (
     FileUploadProcessorProtocol,
 )
+from file_upload.repositories.file_upload_registry_repository import (
+    FileUploadRegistryRepositoryABC,
+)
 
 
 class ProcessFileTaskBase(Task):
-    file_upload_processor_class: type[FileUploadProcessorProtocol]
-    file_registry_manager_class: type[MontrekManager]
-
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(
+        self,
+        *args,
+        file_upload_processor_class: FileUploadProcessorProtocol,
+        file_upload_registry_repository_class: type[FileUploadRegistryRepositoryABC],
+        **kwargs,
+    ) -> None:
         super().__init__()
+        self.file_upload_processor_class = file_upload_processor_class
+        self.file_upload_registry_repository_class = (
+            file_upload_registry_repository_class
+        )
         celery_app.register_task(self)
 
     def run(
@@ -27,25 +37,27 @@ class ProcessFileTaskBase(Task):
         session_data: dict,
         **kwargs,
     ) -> str:
-        # Arguments have to be serializable, so we cannot pass in the processor directly
-        # from the manager.
-        registry_manager = self.file_registry_manager_class(session_data)
-        registry_repo = registry_manager.repository
-        registry = registry_repo.std_queryset().get(pk=file_upload_registry_id)
+        # Arguments have to be serializable so we cannot pass repository,
+        # processor objects from manager.
+        registry_repo = self.file_upload_registry_repository_class(
+            session_data=session_data
+        )
+        registry_obj = registry_repo.std_queryset().get(pk=file_upload_registry_id)
         processor = self.file_upload_processor_class(
-            file_upload_registry_hub=registry, session_data=session_data
+            file_upload_registry_hub=registry_obj, session_data=session_data
         )
         result = processor.process(file_path)
+        result = processor.post_check(file_path) if result else False
         user = get_user_model().objects.get(pk=session_data["user_id"])
         MailingManager(session_data=session_data).send_montrek_mail(
             user.email, "Backgroud File Upload Finished", processor.message
         )
-        att_dict = registry_manager.repository.object_to_dict(registry)
+        att_dict = registry_repo.object_to_dict(registry_obj)
         att_dict.update(
             {
                 "upload_status": "processed" if result else "failed",
                 "upload_message": processor.message,
             },
         )
-        registry = registry_manager.repository.std_create_object(att_dict)
+        registry_obj = registry_repo.std_create_object(att_dict)
         return processor.message
---
+++ montrek/file_upload/tests/tasks/test_process_file_task.py
@@ -0,0 +1,93 @@
+from django.core import mail
+from file_upload.tests.factories.file_upload_factories import (
+    FileUploadRegistryStaticSatelliteFactory,
+)
+from typing import Any
+from django.test import TestCase
+
+from file_upload.tasks.process_file_task import ProcessFileTaskBase
+from file_upload.repositories.file_upload_registry_repository import (
+    FileUploadRegistryRepository,
+)
+from file_upload.models import FileUploadRegistryHubABC
+from user.tests.factories.montrek_user_factories import MontrekUserFactory
+
+
+class DummyFileUploadProcessor:
+    message: str = ""
+
+    def __init__(
+        self,
+        file_upload_registry_hub: FileUploadRegistryHubABC,
+        session_data: dict[str, Any],
+        **kwargs,
+    ):
+        pass
+
+    def pre_check(self, file_path: str) -> bool:
+        return True
+
+    def process(self, file_path: str) -> bool:
+        self.message = f"Successfully processed file '{file_path}'."
+        return True
+
+    def post_check(self, file_path: str) -> bool:
+        return True
+
+
+class ErrorFileUploadProcessor(DummyFileUploadProcessor):
+    def post_check(self, file_path: str) -> bool:
+        self.message = f"Post check failure for '{file_path}'."
+        return False
+
+
+class TestProcessFileTaskBase(TestCase):
+    def setUp(self):
+        self.task = ProcessFileTaskBase(
+            file_upload_processor_class=DummyFileUploadProcessor,
+            file_upload_registry_repository_class=FileUploadRegistryRepository,
+        )
+        self.user = MontrekUserFactory()
+        self.session_data = {"user_id": self.user.id}
+
+    def test_run(self):
+        registry_sat_obj = FileUploadRegistryStaticSatelliteFactory()
+        file_path = f"/dummy/path/{registry_sat_obj.file_name}"
+        result = self.task.delay(
+            file_path=file_path,
+            file_upload_registry_id=registry_sat_obj.id,
+            session_data=self.session_data,
+        )
+        self.assertTrue(result.successful())
+        self.assertEqual(len(mail.outbox), 1)
+        m = mail.outbox[0]
+        self.assertEqual(m.to, [self.user.email])
+        self.assertEqual(m.subject, "Backgroud File Upload Finished")
+        self.assertTrue(
+            f"Successfully processed file '{file_path}'." in m.body,
+        )
+        registry_sat_obj = FileUploadRegistryRepository({}).std_queryset().last()
+        self.assertEqual(registry_sat_obj.upload_status, "processed")
+
+    def test_run_error(self):
+        registry_sat_obj = FileUploadRegistryStaticSatelliteFactory()
+        file_path = f"/dummy/path/{registry_sat_obj.file_name}"
+        self.task = ProcessFileTaskBase(
+            file_upload_processor_class=ErrorFileUploadProcessor,
+            file_upload_registry_repository_class=FileUploadRegistryRepository,
+        )
+        result = self.task.delay(
+            file_path=file_path,
+            file_upload_registry_id=registry_sat_obj.hub_entity.id,
+            session_data=self.session_data,
+        )
+        self.assertTrue(result.successful())
+        self.assertEqual(len(mail.outbox), 1)
+        m = mail.outbox[0]
+        self.assertEqual(m.to, [self.user.email])
+        self.assertEqual(m.subject, "Backgroud File Upload Finished")
+        self.assertTrue(
+            f"Post check failure for '{file_path}'." in m.body,
+        )
+        registry_sat_obj = FileUploadRegistryRepository({}).std_queryset().last()
+        self.assertEqual(registry_sat_obj.upload_status, "failed")
