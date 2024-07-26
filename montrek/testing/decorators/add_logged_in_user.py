45b10fd323d83a68f03a11fc447f4edb93595960
Vincent Mohiuddin <vincent.mohiuddin@2xideas.com>
2024-07-26 17:09:13 +0200

    decorator

---
+++ montrek/testing/decorators/add_logged_in_user.py
@@ -0,0 +1,11 @@
+from user.models import MontrekUser
+from user.tests.factories.montrek_user_factories import MontrekUserFactory
+
+
+def add_logged_in_user(func):
+    def wrapper(self, *args, **kwargs):
+        self.user = MontrekUserFactory()
+        self.client.force_login(self.user)
+        func(self, *args, **kwargs)
+
+    return wrapper
