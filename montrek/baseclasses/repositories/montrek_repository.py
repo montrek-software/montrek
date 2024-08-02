0ddcbca2d3d539e8103d47ffe7e03368292ec5e4
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-08-02 21:27:49 +0200

    Fix test

--- montrek/baseclasses/repositories/montrek_repository.py
+++ montrek/baseclasses/repositories/montrek_repository.py
@@ -164,7 +164,7 @@ class MontrekRepository:
         for _, row in data_frame.iterrows():
             row = row.to_dict()
             for key, value in row.items():
-                if pd.isna(value):
+                if not isinstance(value, (list, dict)) and pd.isna(value):
                     row[key] = None
             hub_entity = self._get_hub_from_data(row)
             created_hub = db_creator.create(row, hub_entity, self.session_user_id)
