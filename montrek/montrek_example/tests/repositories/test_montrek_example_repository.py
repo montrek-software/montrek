63651bc075079f61530eb7421d2fa9ea3fbeb0e9
Vincent Mohiuddin <vincent.mohiuddin@2xideas.com>
2024-08-14 19:32:38 +0200

    order test data to make it db agnostic

--- montrek/montrek_example/tests/repositories/test_montrek_example_repository.py
+++ montrek/montrek_example/tests/repositories/test_montrek_example_repository.py
@@ -1136,8 +1136,9 @@ class TestTimeSeriesStdQueryset(TestCase):
 
     def test_build_time_series_std_queryset(self):
         def make_assertions(test_query):
+            test_query = test_query.order_by("created_at")
             self.assertEqual(test_query.count(), 6)
-            test_obj_0 = test_query[3]
+            test_obj_0 = test_query[0]
             self.assertEqual(test_obj_0.field_c1_str, "Hallo")
             self.assertEqual(test_obj_0.field_c1_bool, True)
             self.assertEqual(test_obj_0.field_tsc2_float, 1.0)
@@ -1145,34 +1146,34 @@ class TestTimeSeriesStdQueryset(TestCase):
             self.assertEqual(test_obj_0.field_tsc3_int, None)
             self.assertEqual(test_obj_0.field_tsc3_str, None)
             test_obj_1 = test_query[1]
-            self.assertEqual(test_obj_1.field_c1_str, "DEFAULT")
+            self.assertEqual(test_obj_1.field_c1_str, "Test")
             self.assertEqual(test_obj_1.field_c1_bool, False)
-            self.assertEqual(test_obj_1.field_tsc2_float, 3.5)
-            self.assertEqual(test_obj_1.value_date, montrek_time(2024, 2, 5).date())
-            self.assertEqual(test_obj_1.field_tsc3_int, 7)
-            self.assertEqual(test_obj_1.field_tsc3_str, "what2")
-            test_obj_2 = test_query[0]
+            self.assertEqual(test_obj_1.field_tsc2_float, 2.5)
+            self.assertEqual(test_obj_1.value_date, montrek_time(2024, 2, 6).date())
+            self.assertEqual(test_obj_1.field_tsc3_int, 5)
+            self.assertEqual(test_obj_1.field_tsc3_str, "what1")
+            test_obj_2 = test_query[2]
             self.assertEqual(test_obj_2.field_c1_str, "Test")
             self.assertEqual(test_obj_2.field_c1_bool, False)
-            self.assertEqual(test_obj_2.field_tsc2_float, 2.5)
-            self.assertEqual(test_obj_2.value_date, montrek_time(2024, 2, 6).date())
-            self.assertEqual(test_obj_2.field_tsc3_int, 5)
-            self.assertEqual(test_obj_2.field_tsc3_str, "what1")
-            test_obj_3 = test_query[2]
-            self.assertEqual(test_obj_3.field_c1_str, "Test")
+            self.assertEqual(test_obj_2.field_tsc2_float, 3.0)
+            self.assertEqual(test_obj_2.value_date, montrek_time(2024, 2, 5).date())
+            self.assertEqual(test_obj_2.field_tsc3_int, None)
+            self.assertEqual(test_obj_2.field_tsc3_str, None)
+            test_obj_3 = test_query[3]
+            self.assertEqual(test_obj_3.field_c1_str, "DEFAULT")
             self.assertEqual(test_obj_3.field_c1_bool, False)
-            self.assertEqual(test_obj_3.field_tsc2_float, 3.0)
+            self.assertEqual(test_obj_3.field_tsc2_float, 3.5)
             self.assertEqual(test_obj_3.value_date, montrek_time(2024, 2, 5).date())
-            self.assertEqual(test_obj_3.field_tsc3_int, None)
-            self.assertEqual(test_obj_3.field_tsc3_str, None)
-            test_obj_4 = test_query[5]
+            self.assertEqual(test_obj_3.field_tsc3_int, 7)
+            self.assertEqual(test_obj_3.field_tsc3_str, "what2")
+            test_obj_4 = test_query[4]
             self.assertEqual(test_obj_4.field_c1_str, "DEFAULT")
             self.assertEqual(test_obj_4.field_c1_bool, False)
             self.assertEqual(test_obj_4.field_tsc2_float, None)
             self.assertEqual(test_obj_4.value_date, None)
             self.assertEqual(test_obj_4.field_tsc3_int, None)
             self.assertEqual(test_obj_4.field_tsc3_str, None)
-            test_obj_5 = test_query[4]
+            test_obj_5 = test_query[5]
             self.assertEqual(test_obj_5.field_c1_str, None)
             self.assertEqual(test_obj_5.field_c1_bool, None)
             self.assertEqual(test_obj_5.field_tsc2_float, None)
