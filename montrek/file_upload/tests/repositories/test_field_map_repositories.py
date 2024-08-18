52e4fb1e43bef55d2b283e09979510b0d179f41c
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-08-18 10:55:46 +0200

    Setup test

--- montrek/file_upload/tests/repositories/test_field_map_repositories.py
+++ montrek/file_upload/tests/repositories/test_field_map_repositories.py
@@ -4,11 +4,13 @@ from file_upload.repositories.field_map_repository import FieldMapRepository
 from file_upload.tests.factories.field_map_factories import (
     FieldMapStaticSatelliteFactory,
 )
+from testing.decorators.add_logged_in_user import add_logged_in_user
 
 
 class TestFieldMapRepository(TestCase):
+    @add_logged_in_user
     def setUp(self):
-        self.repository = FieldMapRepository()
+        self.repository = FieldMapRepository({"user_id": self.user.id})
 
     def test_std_queryset(self):
         field_map_hub = FieldMapStaticSatelliteFactory()
@@ -25,3 +27,17 @@ class TestFieldMapRepository(TestCase):
         )
         source_field = self.repository.get_source_field("database_field")
         self.assertEqual(source_field, "source_field")
+
+    def test_update_existing_field_map(self):
+        FieldMapStaticSatelliteFactory(
+            source_field="source_field", database_field="database_field"
+        )
+        test_queryset = self.repository.std_queryset()
+        self.assertEqual(test_queryset.count(), 1)
+        self.assertEqual(test_queryset.first().source_field, "source_field")
+        self.repository.std_create_object(
+            {"source_field": "new_source_field", "database_field": "database_field"}
+        )
+        test_queryset = self.repository.std_queryset()
+        self.assertEqual(test_queryset.count(), 1)
+        self.assertEqual(test_queryset.first().source_field, "new_source_field")
