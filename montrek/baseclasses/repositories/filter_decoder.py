fc662b9367213919b8f34c8d4177e7f651a4900e
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-08-21 05:54:52 +0200

    Typing

--- montrek/baseclasses/repositories/filter_decoder.py
+++ montrek/baseclasses/repositories/filter_decoder.py
@@ -1,17 +1,22 @@
-from typing import Any
+from typing import TypedDict
 
 from django.db.models import Q
+
+
+class FilterType(TypedDict):
+    filter_value: str | float | int | bool
+    filter_negate: bool
 
 
 class FilterDecoder:
     @staticmethod
     def decode_dict_to_query_list(
-        filter_dict: dict[str, dict[str, str | bool]],
+        filter_dict: dict[str, FilterType | dict[str, FilterType]],
     ) -> list[Q]:
         q_objects = []
         for key, value in filter_dict.items():
             if key.upper() == "OR":
-                q_objects.append(FilterDecoder._append_or_dict(value))
+                q_objects.append(FilterDecoder._append_or_dict(value)
             else:
                 q = Q(**{key: value["filter_value"]})
                 q = ~q if value["filter_negate"] else q
@@ -19,7 +24,7 @@ class FilterDecoder:
         return q_objects
 
     @staticmethod
-    def _append_or_dict(filter_dict) -> Q:
+    def _append_or_dict(filter_dict: dict[str, FilterType]) -> Q:
         query = Q()
         for key, value in filter_dict.items():
             q = Q(**{key: value["filter_value"]})
