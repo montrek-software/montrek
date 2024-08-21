f09111ceecaad2a71c06f9b3409fd546ef56973f
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-08-21 05:50:44 +0200

    Implement or

--- montrek/baseclasses/repositories/filter_decoder.py
+++ montrek/baseclasses/repositories/filter_decoder.py
@@ -5,10 +5,24 @@ from django.db.models import Q
 
 class FilterDecoder:
     @staticmethod
-    def decode_dict_to_query_list(filter_dict: dict[str, Any]):
+    def decode_dict_to_query_list(
+        filter_dict: dict[str, dict[str, str | bool]],
+    ) -> list[Q]:
         q_objects = []
+        for key, value in filter_dict.items():
+            if key.upper() == "OR":
+                q_objects.append(FilterDecoder._append_or_dict(value))
+            else:
+                q = Q(**{key: value["filter_value"]})
+                q = ~q if value["filter_negate"] else q
+                q_objects.append(q)
+        return q_objects
+
+    @staticmethod
+    def _append_or_dict(filter_dict) -> Q:
+        query = Q()
         for key, value in filter_dict.items():
             q = Q(**{key: value["filter_value"]})
             q = ~q if value["filter_negate"] else q
-            q_objects.append(q)
-        return q_objects
+            query |= q
+        return query
--- montrek/baseclasses/tests/repositories/test_filter_decoder.py
+++ montrek/baseclasses/tests/repositories/test_filter_decoder.py
@@ -56,3 +56,77 @@ class TestFilterDecoder(TestCase):
         self.assertEqual(
             test_query_list[0].__dict__, (~Q(field1__exact="value1")).__dict__
         )
+
+    def test_decode_dict_to_filter__multiple_statements_negate(self):
+        filter_dict = {
+            "field1__exact": {
+                "filter_negate": True,
+                "filter_value": "value1",
+            },
+            "field2__exact": {
+                "filter_negate": True,
+                "filter_value": "value2",
+            },
+        }
+        test_query_list = FilterDecoder.decode_dict_to_query_list(filter_dict)
+        self.assertTrue(isinstance(test_query_list, list))
+        self.assertEqual(len(test_query_list), 2)
+        self.assertTrue(isinstance(test_query_list[0], Q))
+        self.assertEqual(
+            test_query_list[0].__dict__, (~Q(field1__exact="value1")).__dict__
+        )
+        self.assertTrue(isinstance(test_query_list[1], Q))
+        self.assertEqual(
+            test_query_list[1].__dict__, (~Q(field2__exact="value2")).__dict__
+        )
+
+    def test_decode_dict_to_filter__mixed_negate(self):
+        filter_dict = {
+            "field1__exact": {
+                "filter_negate": True,
+                "filter_value": "value1",
+            },
+            "field2__exact": {
+                "filter_negate": False,
+                "filter_value": "value2",
+            },
+        }
+        test_query_list = FilterDecoder.decode_dict_to_query_list(filter_dict)
+        self.assertTrue(isinstance(test_query_list, list))
+        self.assertEqual(len(test_query_list), 2)
+        self.assertTrue(isinstance(test_query_list[0], Q))
+        self.assertEqual(
+            test_query_list[0].__dict__, (~Q(field1__exact="value1")).__dict__
+        )
+        self.assertTrue(isinstance(test_query_list[1], Q))
+        self.assertEqual(
+            test_query_list[1].__dict__, Q(field2__exact="value2").__dict__
+        )
+
+    def test_decode_dict_to_filter__empty(self):
+        filter_dict = {}
+        test_query_list = FilterDecoder.decode_dict_to_query_list(filter_dict)
+        self.assertTrue(isinstance(test_query_list, list))
+        self.assertEqual(len(test_query_list), 0)
+
+    def test_decode_dict_to_filter__or_statement(self):
+        filter_dict = {
+            "or": {
+                "field1__exact": {
+                    "filter_negate": False,
+                    "filter_value": "value1",
+                },
+                "field2__exact": {
+                    "filter_negate": False,
+                    "filter_value": "value2",
+                },
+            }
+        }
+        test_query_list = FilterDecoder.decode_dict_to_query_list(filter_dict)
+        self.assertTrue(isinstance(test_query_list, list))
+        self.assertEqual(len(test_query_list), 1)
+        self.assertTrue(isinstance(test_query_list[0], Q))
+        self.assertEqual(
+            test_query_list[0].__dict__,
+            (Q(field1__exact="value1") | Q(field2__exact="value2")).__dict__,
+        )
