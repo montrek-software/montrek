9b32fe07a7a876029062d0b236d2e7caa997b977
chrishombach <chris.hombach@gmail.com>
2024-10-17 08:30:36 +0200

    Force env file (#524)
    * Show error message
    
    * Companu IDentifier API
    
    * Force env file path
    
    ---------
    
    Co-authored-by: Christoph Hombach <Christoph.Hombach@2xideas.com>

--- montrek/montrek/settings.py
+++ montrek/montrek/settings.py
@@ -12,11 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-from decouple import config
+from decouple import Config, RepositoryEnv
 from django.urls import reverse_lazy
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+config = Config(RepositoryEnv(BASE_DIR / "../.env"))
 
 
 # Quick-start development settings - unsuitable for production
