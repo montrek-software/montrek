9364521b522515cae77841ca2e7fced542c06bf4
Vincent Mohiuddin <vincent.mohiuddin@2xideas.com>
2024-12-09 09:41:08 +0100

    link transaction asset

--- montrek/showcase/models/stransaction_hub_models.py
+++ montrek/showcase/models/stransaction_hub_models.py
@@ -5,11 +5,21 @@ from baseclasses.models import (
     MontrekHubABC,
     MontrekOneToManyLinkABC,
 )
+from showcase.models.sasset_hub_models import SAssetHub
 from showcase.models.sproduct_hub_models import SProductHub
 
 
 class STransactionHub(MontrekHubABC):
-    pass
+    link_stransaction_sproduct = models.ManyToManyField(
+        SProductHub,
+        through="LinkSTransactionSProduct",
+        related_name="link_sproduct_stransaction",
+    )
+    link_stransaction_sasset = models.ManyToManyField(
+        SAssetHub,
+        through="LinkSTransactionSAsset",
+        related_name="link_sasset_stransaction",
+    )
 
 
 class STransactionHubValueDate(HubValueDate):
@@ -19,3 +29,8 @@ class STransactionHubValueDate(HubValueDate):
 class LinkSTransactionSProduct(MontrekOneToManyLinkABC):
     hub_in = models.ForeignKey(STransactionHub, on_delete=models.CASCADE)
     hub_out = models.ForeignKey(SProductHub, on_delete=models.CASCADE)
+
+
+class LinkSTransactionSAsset(MontrekOneToManyLinkABC):
+    hub_in = models.ForeignKey(STransactionHub, on_delete=models.CASCADE)
+    hub_out = models.ForeignKey(SAssetHub, on_delete=models.CASCADE)
