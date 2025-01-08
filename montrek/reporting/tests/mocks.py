b30d376f849fe662afee32f58acba2c1921335c3
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-01-08 16:17:42 +0100

    Setup failing test

--- montrek/reporting/tests/mocks.py
+++ montrek/reporting/tests/mocks.py
@@ -1,8 +1,14 @@
+import pandas as pd
+
+from reporting.constants import ReportingPlotType
+from reporting.core import reporting_text
+from reporting.core.reporting_data import ReportingData
+from reporting.core.reporting_grid_layout import ReportGridLayout
+from reporting.core.reporting_plots import ReportingPlot
+from reporting.managers.latex_report_manager import LatexReportManager
 from reporting.managers.montrek_report_manager import (
     MontrekReportManager,
 )
-from reporting.managers.latex_report_manager import LatexReportManager
-from reporting.core import reporting_text
 
 
 class MockNoCollectReportElements(MontrekReportManager):
@@ -37,3 +43,30 @@ class MockComprehensiveReportManager(MontrekReportManager):
 
     def collect_report_elements(self):
         self.append_report_element(reporting_text.ReportingHeader1("Hallo"))
+        self.append_report_element(self.grid())
+
+    def grid(self):
+        grid = ReportGridLayout(2, 2)
+        grid.add_report_grid_element(reporting_text.ReportingParagraph("One"), 0, 0)
+        grid.add_report_grid_element(reporting_text.ReportingParagraph("Two"), 0, 1)
+        grid.add_report_grid_element(self.plot(), 1, 0)
+        grid.add_report_grid_element(reporting_text.ReportingParagraph("Four"), 1, 1)
+        return grid
+
+    def plot(self):
+        test_df = pd.DataFrame(
+            {
+                "Category": ["A", "B", "C", "D"],
+                "Value": [10, 25, 15, 30],
+            },
+        )
+        reporting_data = ReportingData(
+            data_df=test_df,
+            x_axis_column="Category",
+            y_axis_columns=["Value"],
+            plot_types=[ReportingPlotType.PIE],
+            title="Test Plot",
+        )
+        reporting_plot = ReportingPlot()
+        reporting_plot.generate(reporting_data)
+        return reporting_plot
