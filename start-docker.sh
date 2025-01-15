23444b02b70d230dd9262e3eb5d7615d79ce7279
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-01-15 16:21:59 +0100

    Add docker start script

---
+++ start-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Base docker-compose.yml file
+MAIN_COMPOSE_FILE="docker-compose.yml"
+
+# Create an array to store found docker-compose files
+COMPOSE_FILES=("$MAIN_COMPOSE_FILE")
+
+# Find all docker-compose.yml files in subdirectories
+while IFS= read -r -d '' file; do
+	COMPOSE_FILES+=(" -f $file")
+done < <(find . -mindepth 2 -name "docker-compose.yml" -print0)
+
+# Print the compose files to be used
+echo "Detected docker-compose files: ${COMPOSE_FILES[*]}"
+
+# Combine and run them
+docker compose -f ${COMPOSE_FILES[@]} up
