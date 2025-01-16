7f1883984e68dfc8e822c83bd4a6b81a09a5c741
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-01-16 06:55:40 +0100

    Add -d flag

--- start-docker.sh
+++ start-docker.sh
@@ -14,5 +14,10 @@ done < <(find . -mindepth 2 -name "docker-compose.yml" -print0)
 # Print the compose files to be used
 echo "Detected docker-compose files: ${COMPOSE_FILES[*]}"
 
+# Check for the -d flag
+DETACHED=""
+if [[ "$1" == "-d" ]]; then
+	DETACHED="-d"
+fi
 # Combine and run them
-docker compose -f ${COMPOSE_FILES[@]} up
+docker compose -f ${COMPOSE_FILES[@]} up $DETACHED
