160739c87c236b436726f794809e1feb09471fc5
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-01-22 12:02:59 +0100

    Add Latex Bar

--- montrek/reporting/dataclasses/table_elements.py
+++ montrek/reporting/dataclasses/table_elements.py
@@ -265,8 +265,8 @@ class ProgressBarTableElement(NumberTableElement):
         return f"""<td><div class="bar-container"> <div class="bar" style="width: {per_value}%;"></div> <span class="bar-value">{value:,.2%}</span> </div></td>"""
 
     def format_latex(self, value) -> str:
-        value = super().format_latex(value)
-        return value.replace("%", "\\%")
+        per_value = value * 100
+        return f"\\progressbar{{ {per_value } }}{{ {per_value}\\% }} &"
 
 
 @dataclass
