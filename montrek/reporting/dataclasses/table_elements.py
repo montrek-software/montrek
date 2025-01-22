147cf5058f964d98157b6264708a4aa87d80c861
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-01-22 11:29:03 +0100

    Add ProgressBarTableElement

--- montrek/reporting/dataclasses/table_elements.py
+++ montrek/reporting/dataclasses/table_elements.py
@@ -256,6 +256,20 @@ class PercentTableElement(NumberTableElement):
 
 
 @dataclass
+class ProgressBarTableElement(NumberTableElement):
+    serializer_field_class = serializers.FloatField
+    attr: str
+
+    def _format_value(self, value) -> str:
+        per_value = value * 100
+        return f"""<div class="bar-container"> <div class="bar" style="width: {per_value}%;"></div> <span class="bar-value">{value:,.2%}</span> </div>"""
+
+    def format_latex(self, value) -> str:
+        value = super().format_latex(value)
+        return value.replace("%", "\\%")
+
+
+@dataclass
 class DateTableElement(AttrTableElement):
     serializer_field_class = serializers.DateField
     attr: str
