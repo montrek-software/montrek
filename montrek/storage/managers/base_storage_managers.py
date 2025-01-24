c2430d821e92870aa010566784ed84cac4430f4f
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-01-24 16:39:26 +0100

    Firs TmpStorageManager implementation

--- montrek/storage/managers/base_storage_managers.py
+++ montrek/storage/managers/base_storage_managers.py
@@ -3,9 +3,13 @@ from abc import ABC, abstractmethod
 
 class BaseStorageManager(ABC):
     @abstractmethod
-    def save(self, file_path: str):
+    def save(self, file_path: str) -> str:
         pass
 
     @abstractmethod
-    def load(self) -> bytes:
+    def load(self, file_name: str) -> bytes:
+        pass
+
+    @abstractmethod
+    def cleanup(self):
         pass
---
+++ montrek/storage/tests/managers/test_tmp_storage_manager.py
@@ -0,0 +1,18 @@
+import os
+from django.test import TestCase
+from storage.managers.tmp_storage_manager import TmpStorageManager
+
+
+class TestTmpStorageManager(TestCase):
+    def test_save_and_cleanup(self):
+        test_file_name = "test.txt"
+        with open(test_file_name, "w") as f:
+            f.write("test")
+        tmp_storage_manager = TmpStorageManager()
+        saved_path = tmp_storage_manager.save(test_file_name)
+        self.assertTrue(os.path.exists(saved_path))
+        tmp_storage_manager.cleanup()
+        self.assertFalse(os.path.exists(saved_path))
+
+    def test_load(self):
+        pass
