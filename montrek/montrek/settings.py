cb493d1d7f139007db3178b8be587ebc71ad0ca7
Vincent Mohiuddin <vincent.mohiuddin@2xideas.com>
2025-01-27 11:47:07 +0100

    import

--- montrek/montrek/settings.py
+++ montrek/montrek/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 from decouple import Config, RepositoryEnv
 from django.urls import reverse_lazy
 
+import montrek.filtered_warnings  # noqa
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
