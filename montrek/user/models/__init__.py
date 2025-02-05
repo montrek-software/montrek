613443338e68f7a5dc66f2d8d09a22b102beccb0
Vincent Mohiuddin <vincent.mohiuddin@2xideas.com>
2025-02-05 14:06:24 +0100

    naming

--- montrek/user/models/__init__.py
+++ montrek/user/models/__init__.py
@@ -1,5 +1,5 @@
 from .montrek_user_hub_models import MontrekUserHub
 from .montrek_user_sat_models import MontrekUserSatellite
-from .montrek_user_models import MontrekUser
+from .montrek_user_auth_models import MontrekUser
 
 __all__ = ["MontrekUserHub", "MontrekUserSatellite", "MontrekUser"]
