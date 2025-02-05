f4b2a841dc6ea40d4078c76f09e7abb770a622c9
Vincent Mohiuddin <vincent.mohiuddin@2xideas.com>
2025-02-05 14:05:35 +0100

    fix imports

---
+++ montrek/user/models/__init__.py
@@ -0,0 +1,5 @@
+from .montrek_user_hub_models import MontrekUserHub
+from .montrek_user_sat_models import MontrekUserSatellite
+from .montrek_user_models import MontrekUser
+
+__all__ = ["MontrekUserHub", "MontrekUserSatellite", "MontrekUser"]
