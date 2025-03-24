93cadb535778ace74f9c0b3bec7ca8c80bf7dd56
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-03-24 14:12:02 +0100

    Fix imports

--- montrek/data_import/api_import/tests/factories/api_data_import_registry_factories.py
+++ montrek/data_import/api_import/tests/factories/api_data_import_registry_factories.py
@@ -1,5 +1,5 @@
 from data_import.base.tests.factories.data_import_registry_factories import DataImportRegistryBaseSatelliteFactory
-from data_import.api_import.models.api_registry_sat_models import MockApiRegistrySatellite
+from data_import.api_import.models import MockApiRegistrySatellite
 import factory
 
 class ApiDataImportRegistryBaseSatelliteFactory(DataImportRegistryBaseSatelliteFactory):
