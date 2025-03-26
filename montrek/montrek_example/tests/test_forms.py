c11f96efaa61744631393b9064644fcff2bf75b1
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-03-26 18:10:25 +0100

    Form requires link

--- montrek/montrek_example/tests/test_forms.py
+++ montrek/montrek_example/tests/test_forms.py
@@ -5,6 +5,13 @@ from montrek_example.repositories.hub_a_repository import HubARepository
 
 
 class TestMontrekCreateForm(TestCase):
-    def test_link_choice_char_field(self):
+    def test_link_choice_char_field__expect_link_id(self):
         repository = HubARepository({})
-        test_form = ExampleACreateForm(repository=repository)
+        test_data = {
+            "field_a1_str": "test",
+            "field_a1_int": 1,
+            "field_a2_str": "test2",
+            "field_a2_float": 2.0,
+        }
+        test_form = ExampleACreateForm(data=test_data, repository=repository)
+        self.assertFalse(test_form.is_valid())
