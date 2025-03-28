30c59be97a9f2d30d880ef9a879944a8e968e16e
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-03-28 11:57:00 +0100

    Move Filter code to external class

---
+++ montrek/baseclasses/typing.py
@@ -0,0 +1,3 @@
+from typing import Any
+
+SessionDataType = dict[str, Any]
--- montrek/baseclasses/utils.py
+++ montrek/baseclasses/utils.py
@@ -1,7 +1,10 @@
 import datetime
 from datetime import timedelta
 from typing import Tuple
+
 from django.utils import timezone
+
+from baseclasses.typing import SessionDataType
 
 # TODO Make universal MontrekDateTime class
 
@@ -64,3 +67,86 @@ def get_content_type(filename: str) -> str:
     if file_extension == "zip":
         return "application/zip"
     return "application/octet-stream"
+
+
+class TableMetaSessionData:
+    def __init__(self, request) -> None:
+        self.request = request
+
+    def update_session_data(self, session_data: SessionDataType) -> SessionDataType:
+        session_data.update(self._get_filters(session_data))
+        session_data.update(self._get_page_number(session_data))
+        session_data.update(self._get_filter_form_count(session_data))
+        self.request.session["filter"] = session_data.get("filter", {})
+        self.request.session["pages"] = session_data.get("pages", {})
+        self.request.session["filter_count"] = session_data.get("filter_count", {})
+        return session_data
+
+    def _get_filters(self, session_data):
+        request_path = self.request.path
+        filter_data = {"filter": session_data.pop("filter", {})}
+
+        filter_fields = session_data.pop("filter_field", [])
+        filter_negates = session_data.pop("filter_negate", [""] * len(filter_fields))
+        filter_lookups = session_data.pop("filter_lookup", [])
+        filter_values = session_data.pop("filter_value", [""] * len(filter_fields))
+        filter_input_data = list(
+            zip(filter_fields, filter_negates, filter_lookups, filter_values)
+        )
+        if len(filter_input_data) > 0:
+            filter_data["filter"][request_path] = {}
+        for (
+            filter_field,
+            filter_negate,
+            filter_lookup,
+            filter_value,
+        ) in filter_input_data:
+            if filter_lookup == "isnull":
+                filter_value = True
+            if filter_field:
+                true_values = ("True", "true", True)
+                false_values = ("False", "false", False)
+                filter_negate = filter_negate in true_values
+                filter_lookup = filter_lookup
+                filter_value = filter_value
+                filter_key = f"{filter_field}__{filter_lookup}"
+                if filter_lookup == "in":
+                    filter_value = filter_value.split(",")
+                if filter_value in true_values:
+                    filter_value = True
+                elif filter_value in false_values:
+                    filter_value = False
+
+                filter_data["filter"][request_path][filter_key] = {
+                    "filter_negate": filter_negate,
+                    "filter_value": filter_value,
+                }
+        return filter_data
+
+    def _get_page_number(self, session_data):
+        request_path = self.request.path
+        pages_data = {}
+        if "pages" not in session_data:
+            pages_data["pages"] = {}
+            session_data["pages"] = {}
+        else:
+            pages_data["pages"] = session_data["pages"]
+        if "page" in session_data:
+            page = session_data["page"]
+            pages_data["pages"][request_path] = page
+        else:
+            if request_path in session_data["pages"]:
+                pages_data["page"] = session_data["pages"][request_path]
+        return pages_data
+
+    def _get_filter_form_count(self, session_data):
+        request_path = self.request.path
+        cfield = "filter_count"
+        count_data = {}
+        if cfield not in session_data:
+            count_data[cfield] = {}
+        else:
+            count_data[cfield] = session_data[cfield]
+        if request_path not in count_data[cfield]:
+            count_data[cfield][request_path] = 1
+        return count_data
