a1ea69fbe405fb68bc67251b89ee1e21ed781bf4
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-04-04 13:17:51 +0200

    Use Mock classes for tests

--- montrek/info/tests/managers/test_info_versions_managers.py
+++ montrek/info/tests/managers/test_info_versions_managers.py
@@ -1,19 +1,6 @@
 import pandas as pd
 from django.test import TestCase
-from info.managers.info_versions_managers import InfoVersionsManager
-from info.modules.git_info import GitInfo
-
-
-class MockGitInfo(GitInfo):
-    def to_dataframe(self) -> pd.DataFrame:
-        return pd.DataFrame({"repository": ["A", "B", "C"]})
-
-    def _check_is_git_repo(self) -> bool:
-        return True
-
-
-class MockInfoVersionsManager(InfoVersionsManager):
-    git_info_class = MockGitInfo
+from info.tests.mocks import MockInfoVersionsManager
 
 
 class TestInfoVersionsManager(TestCase):
--- montrek/info/tests/views/test_info_views.py
+++ montrek/info/tests/views/test_info_views.py
@@ -1,8 +1,8 @@
 from testing.test_cases.view_test_cases import MontrekReportViewTestCase
-from info.views.info_views import InfoVersionsView
+from info.tests.mocks import MockInfoVersionsView
 
 
 class TestInfoVersionsView(MontrekReportViewTestCase):
     expected_number_of_report_elements = 1
-    view_class = InfoVersionsView
+    view_class = MockInfoVersionsView
     viewname = "info"
