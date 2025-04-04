be62d47757a4c4f95fa882048b8c73cd20eee070
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-04-04 10:01:07 +0200

    Show git Data

--- montrek/info/managers/info_versions_managers.py
+++ montrek/info/managers/info_versions_managers.py
@@ -2,12 +2,23 @@ import pandas as pd
 from reporting.managers.montrek_report_manager import MontrekReportManager
 from reporting.managers.montrek_table_manager import MontrekDataFrameTableManager
 from reporting.dataclasses import table_elements as te
+from info.modules.git_info import GitInfo
 
 
 class GitVersionsManager(MontrekDataFrameTableManager):
     @property
     def table_elements(self) -> tuple[te.TableElement, ...]:
-        return (te.StringTableElement(name="repository", attr="repository"),)
+        return (
+            te.StringTableElement(name="repo_name", attr="repo_name"),
+            te.StringTableElement(name="latest_tag", attr="latest_tag"),
+            te.StringTableElement(name="branch", attr="branch"),
+            te.DateTimeTableElement(name="commit_hash", attr="commit_hash"),
+            te.StringTableElement(name="author", attr="author"),
+            te.StringTableElement(name="commit_date", attr="commit_date"),
+            te.StringTableElement(name="remote_url", attr="remote_url"),
+            te.StringTableElement(name="commit_count", attr="commit_count"),
+            te.BooleanTableElement(name="is_clean", attr="is_clean"),
+        )
 
 
 class InfoVersionsManager(MontrekReportManager):
@@ -17,7 +28,7 @@ class InfoVersionsManager(MontrekReportManager):
         self.append_report_element(self.get_git_versions())
 
     def get_git_versions(self) -> MontrekDataFrameTableManager:
-        git_versions_df = pd.DataFrame({"repository": ["A", "B", "C"]})
+        git_versions_df = GitInfo(".").to_dataframe()
         session_data = self.session_data | {
             "df_data": git_versions_df.to_dict(orient="records")
         }
