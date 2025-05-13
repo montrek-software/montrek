f1a891317a105d96cf873add98fc7a5fbe9f8a5a
Vincent Mohiuddin <vincent.mohiuddin@montrek-software.de>
2025-05-13 10:53:45 +0200

    draft

--- bin/start-docker.sh
+++ bin/start-docker.sh
@@ -8,7 +8,7 @@ COMPOSE_FILES=("$MAIN_COMPOSE_FILE")
 
 # Find all docker-compose.yml files in subdirectories
 while IFS= read -r -d '' file; do
-	COMPOSE_FILES+=(" -f $file")
+  COMPOSE_FILES+=(" -f $file")
 done < <(find . -mindepth 2 -name "docker-compose.yml" -print0)
 
 # Print the compose files to be used
@@ -19,13 +19,23 @@ echo "Detected docker-compose files: ${COMPOSE_FILES[*]}"
 COMMAND="up"
 
 if [[ "$1" == "down" ]]; then
-	COMMAND="down"
+  COMMAND="down"
 fi
 
 # Check for the -d flag
 DETACHED=""
 if [[ "$2" == "-d" ]]; then
-	DETACHED="-d"
+  DETACHED="-d"
 fi
+
+BUILD=""
+if [[ "$3" == "--build" ]]; then
+  BUILD="--build"
+fi
+PRUNE=""
+if [[ "$4" == "--prune" ]]; then
+  PRUNE="--prune"
+fi
+
 # Combine and run them
-docker compose -f ${COMPOSE_FILES[@]} $COMMAND $DETACHED
+docker compose -f "${COMPOSE_FILES[@]}" $COMMAND $DETACHED $BUILD $PRUNE
