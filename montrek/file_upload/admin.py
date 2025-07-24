1c41f468b48953ef7c5ab5de3ce1fc0a1b74e524
chrishombach <chris.hombach@gmail.com>
2025-07-24 20:31:29 +0200

    965 admin afterworks (#966)
    * Add django authentficator
    
    * Remove admin tables
    
    ---------
    
    Co-authored-by: Christoph Hombach <Christoph.Hombach@2xideas.com>

--- montrek/file_upload/admin.py
+++ montrek/file_upload/admin.py
@@ -1,10 +1,1 @@
-from django.contrib import admin
-from .models import FieldMapHub, FieldMapStaticSatellite, FileUploadFileHub
-from .models import FileUploadFileStaticSatellite
-
 # Register your models here.
-
-admin.site.register(FileUploadFileHub)
-admin.site.register(FileUploadFileStaticSatellite)
-admin.site.register(FieldMapHub)
-admin.site.register(FieldMapStaticSatellite)
