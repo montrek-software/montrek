9cca1f04fbd255ac0ade56e3ed0dddd6538dddec
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-07-24 14:31:22 +0200

    Add links

--- montrek/info/managers/info_admin_managers.py
+++ montrek/info/managers/info_admin_managers.py
@@ -1,9 +1,20 @@
 import pandas as pd
+from django.conf import settings
+
+from baseclasses.typing import TableElementsType
+from reporting.dataclasses import table_elements as te
 from reporting.managers.montrek_report_manager import MontrekReportManager
 from reporting.managers.montrek_table_manager import MontrekDataFrameTableManager
 
 
-class AdminLinksManager(MontrekDataFrameTableManager): ...
+class AdminLinksManager(MontrekDataFrameTableManager):
+    @property
+    def table_elements(self) -> TableElementsType:
+        return (
+            te.StringTableElement(name="System", attr="system"),
+            te.StringTableElement(name="Description", attr="description"),
+            te.ExternalLinkTableElement(name="Link", attr="link"),
+        )
 
 
 class InfoAdminManager(MontrekReportManager):
@@ -13,7 +24,22 @@ class InfoAdminManager(MontrekReportManager):
         self.append_report_element(self.get_admin_links())
 
     def get_admin_links(self):
-        admin_links_df = pd.DataFrame()
+        deploy_host = settings.DEPLOY_HOST
+        project_name = settings.PROJECT_NAME
+        django_admin_link = f"https://{project_name}.{deploy_host}/admin"
+        keycloak_link = f"https://auth.{project_name}.{deploy_host}"
+        flower_link = f"https://flower.{project_name}.{deploy_host}"
+        admin_links_df = pd.DataFrame(
+            {
+                "system": ["Django Admin", "Keycloak", "Flower"],
+                "description": [
+                    "User Management & Scheduling",
+                    "Authentification",
+                    "Task Monitoring",
+                ],
+                "link": [django_admin_link, keycloak_link, flower_link],
+            }
+        )
         session_data = self.session_data | {
             "df_data": admin_links_df.to_dict(orient="records")
         }
