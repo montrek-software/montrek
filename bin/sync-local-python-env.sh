6fead832905f08f863c7b98a13d9856ee6e35278
chrishombach <chris.hombach@gmail.com>
2025-08-06 14:58:34 +0200

    981 shorten sync script to match with init script (#982)
    * Setup env in init step
    
    * Compile requirement.in first
    
    * Run init
    
    * Set rights right
    
    * call all-requirements.txt
    
    * move to lightweight latex
    
    * Get package right
    
    * Install all needed packages
    
    * Revert to old Latex installation
    
    * Add requirements cache
    
    ---------
    
    Co-authored-by: Christoph Hombach <Christoph.Hombach@2xideas.com>

--- bin/sync-local-python-env.sh
+++ bin/sync-local-python-env.sh
@@ -1,37 +1,10 @@
 #!/bin/bash
-# Load pyenv and pyenv-virtualenv
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-# Define the pyenv version name and Python version
-pyenv_version_name="montrek-3.12.0"
-python_version="3.12.0"
-
-# Check if the pyenv version exists
-if ! pyenv versions | grep -q "$pyenv_version_name"; then
-	echo "Pyenv version $pyenv_version_name not found. Creating it with Python $python_version..."
-	pyenv install -s "$python_version"
-	pyenv virtualenv "$python_version" "$pyenv_version_name"
-else
-	echo "Pyenv version $pyenv_version_name already exists."
-fi
-
-# Check if the pyenv version is activated
-current_pyenv_version=$(pyenv version-name)
-if [ "$current_pyenv_version" != "$pyenv_version_name" ]; then
-	echo "Activating pyenv version $pyenv_version_name..."
-	echo "pyenv activate $pyenv_version_name"
-	pyenv activate "$pyenv_version_name"
-else
-	echo "Pyenv version $pyenv_version_name is already active."
-fi
 
 # Install pip-tools
 echo "Installing pip-tools..."
 pip install pip-tools
-temporary_requirements_file="all_requirements.txt"
-find . -name 'requirements.txt' -exec cat {} + >"$temporary_requirements_file"
-pip-sync "$temporary_requirements_file"
+temporary_requirements_file="all_requirements.in"
+find . -name 'requirements.in' -exec cat {} + >"$temporary_requirements_file"
+pip-compile "$temporary_requirements_file"
+pip-sync all_requirements.txt
 rm "$temporary_requirements_file"
