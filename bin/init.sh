c63448f1e0f9b5b213b4b6beca87e74ed47fff09
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-08-06 10:57:16 +0200

    Setup env in init step

--- bin/init.sh
+++ bin/init.sh
@@ -6,6 +6,34 @@ PROJECT_NAME=$(basename "$PWD")
 ENV_NAME="$PROJECT_NAME-$PYTHON_VERSION"
 echo "$ENV_NAME" >.python-version
 
+# Load pyenv and pyenv-virtualenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+# Define the pyenv version name and Python version
+pyenv_version_name=$ENV_NAME
+python_version=PYTHON_VERSION
+
+# Check if the pyenv version exists
+if ! pyenv versions | grep -q "$pyenv_version_name"; then
+  echo "Pyenv version $pyenv_version_name not found. Creating it with Python $python_version..."
+  pyenv install -s "$python_version"
+  pyenv virtualenv "$python_version" "$pyenv_version_name"
+else
+  echo "Pyenv version $pyenv_version_name already exists."
+fi
+
+# Check if the pyenv version is activated
+current_pyenv_version=$(pyenv version-name)
+if [ "$current_pyenv_version" != "$pyenv_version_name" ]; then
+  echo "Activating pyenv version $pyenv_version_name..."
+  echo "pyenv activate $pyenv_version_name"
+  pyenv activate "$pyenv_version_name"
+else
+  echo "Pyenv version $pyenv_version_name is already active."
+fi
 # Check if virtualenv already exists
 if pyenv virtualenvs --bare | grep -q "^$ENV_NAME$"; then
   echo "Virtualenv '$ENV_NAME' already exists. Skipping creation."
