40d079dcec91ad2ddbb4a8ea1d861ea9141531fa
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-08-08 06:40:59 +0200

    Collect certs correctly

--- bin/entrypoints/montrek-entrypoint.sh
+++ bin/entrypoints/montrek-entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 echo "SERVICE is: $SERVICE"
 
 # Install instance-specific certs if provided
-if [[ -f /certs/fullchain.crt ]]; then
+if [[ -f ./nginx/certs/fullchain.crt ]]; then
   echo "Installing instance-specific certificate..."
-  cp /certs/fullchain.crt /usr/local/share/ca-certificates/montrek_root_ca.crt
+  cp ./nginx/certs/fullchain.crt /usr/local/share/ca-certificates/montrek_root_ca.crt
   update-ca-certificates
 fi
 # Conditionally run the app as appuser only for the "web" role
