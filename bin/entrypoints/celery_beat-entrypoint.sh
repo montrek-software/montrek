b15c819ccc842385d9670cd912eb911f77bab59f
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-08-13 20:39:51 +0200

    Sync python  env in worker and celery beat

--- bin/entrypoints/celery_beat-entrypoint.sh
+++ bin/entrypoints/celery_beat-entrypoint.sh
@@ -3,4 +3,5 @@ set -e
 
 . .venv/bin/activate
 cd montrek
+make sync-local-python-env
 python -m celery --app=montrek beat --loglevel=info
--- bin/entrypoints/worker-entrypoint.sh
+++ bin/entrypoints/worker-entrypoint.sh
@@ -3,4 +3,5 @@ set -e
 
 . .venv/bin/activate
 cd montrek
+make sync-local-python-env
 python -m celery --app=montrek worker --loglevel=info -Q sequential_queue --concurrency=$1 -n $2@%h
