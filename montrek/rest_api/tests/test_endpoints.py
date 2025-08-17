1b3c6f0b7ae832304a22d58436b2ec69b9e0a4b7
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-08-17 12:39:50 +0200

    Add app and tokens call

---
+++ montrek/rest_api/tests/test_endpoints.py
@@ -0,0 +1,22 @@
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+
+class TokenObtainPairTests(APITestCase):
+    def test_endpoints_not_redirected_to_user_login(self):
+        for reverse_name in ("token_obtain_pair", "token_refresh", "token_verify"):
+            with self.subTest(endpoint=reverse_name):
+                url = reverse(reverse_name)
+                resp = self.client.post(url, {}, format="json")
+                # Not redirected to /user/login/ via Response.url attribute
+                self.assertNotEqual(getattr(resp, "url", None), "/user/login/")
+
+                # Not redirected to /user/login/ via Location header
+                self.assertFalse(
+                    resp.has_header("Location")
+                    and resp["Location"].startswith("/user/login/"),
+                    msg=f"{reverse_name} unexpectedly redirected to login.",
+                )
+
+                # Not a redirect status
+                self.assertNotIn(resp.status_code, (301, 302, 303, 307, 308))
