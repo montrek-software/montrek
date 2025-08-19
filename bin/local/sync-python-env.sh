274bd1e485e75c7fc5a9fb74176aa2cab63f4e8e
chrishombach <chris.hombach@gmail.com>
2025-08-19 09:21:21 +0200

    Add newline to all_requirements.in (#1021)
    Co-authored-by: Christoph Hombach <Christoph.Hombach@2xideas.com>

--- bin/local/sync-python-env.sh
+++ bin/local/sync-python-env.sh
@@ -7,7 +7,8 @@ uv venv
 
 # Combine all requirements.in files into one
 temporary_requirements_file="all_requirements.in"
-find . -name 'requirements.in' -exec cat {} + >"$temporary_requirements_file"
+>"$temporary_requirements_file"
+find . -name 'requirements.in' -exec sh -c 'cat "$1"; echo' _ {} \; >>"$temporary_requirements_file"
 
 # Compile and sync using uv
 uv pip compile "$temporary_requirements_file" --output-file requirements.txt
