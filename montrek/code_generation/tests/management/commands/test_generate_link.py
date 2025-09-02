024c34274ec39191bc5c6082067e291a665331ea
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-09-02 17:27:42 +0200

    rm test dirs and add helper

--- montrek/code_generation/tests/management/commands/test_generate_link.py
+++ montrek/code_generation/tests/management/commands/test_generate_link.py
@@ -9,19 +9,19 @@ from django.test import TestCase
 class TestGenerateLinkCommand(TestCase):
     def setUp(self):
         self.maxDiff = None
-        self.output_dir = os.path.relpath(get_test_file_path("output"))
 
     def tearDown(self) -> None:
         shutil.rmtree(self.output_dir)
 
     def test_insertions_in_files(self):
+        self.output_dir = os.path.relpath(get_test_file_path("output"))
         os.makedirs(self.output_dir, exist_ok=True)
-        # with patch("sys.stdout", new_callable=io.StringIO):
-        call_command("generate_table", self.output_dir, "mother")
-        call_command("generate_table", self.output_dir, "daughter")
-        call_command(
-            "generate_link", self.output_dir, "daughter", self.output_dir, "mother"
-        )
+        with patch("sys.stdout", new_callable=io.StringIO):
+            call_command("generate_table", self.output_dir, "mother")
+            call_command("generate_table", self.output_dir, "daughter")
+            call_command(
+                "generate_link", self.output_dir, "daughter", self.output_dir, "mother"
+            )
         with open(
             os.path.join(self.output_dir, "models", "daughter_hub_models.py")
         ) as f:
@@ -37,3 +37,4 @@ class TestGenerateLinkCommand(TestCase):
     hub_in = models.ForeignKey(DaughterHub, on_delete=models.CASCADE)
     hub_out = models.ForeignKey(MotherHub, on_delete=models.CASCADE)"""
             self.assertIn(link_class.replace(" ", ""), code)
+        shutil.rmtree(self.output_dir)
