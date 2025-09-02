35501044621d8596f59114bfe7e67477fbcf0489
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-09-02 15:08:54 +0200

    Setup test

---
+++ montrek/code_generation/tests/management/commands/test_generate_link.py
@@ -0,0 +1,26 @@
+import io
+import os
+import shutil
+from unittest.mock import patch
+
+from code_generation.tests import get_test_file_path
+from django.core.management import call_command
+from django.test import TestCase
+
+
+class TestGenerateLinkCommand(TestCase):
+    def setUp(self):
+        self.maxDiff = None
+        self.output_dir = os.path.relpath(get_test_file_path("output"))
+
+    def tearDown(self) -> None:
+        shutil.rmtree(self.output_dir)
+
+    def test_insertions_in_files(self):
+        os.makedirs(self.output_dir, exist_ok=True)
+        with patch("sys.stdout", new_callable=io.StringIO):
+            call_command("generate_table", self.output_dir, "mother")
+            call_command("generate_table", self.output_dir, "daughter")
+            call_command(
+                "generate_link", self.output_dir, "daughter", self.output_dir, "mother"
+            )
