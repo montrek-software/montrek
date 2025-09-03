dd97bd0e745b8299d088f8003cb1f74e3c8db6ef
chrishombach <chris.hombach@gmail.com>
2025-09-03 15:32:09 +0200

    1059 generate test for list (#1060)
    * Add command
    
    * Create links first
    
    * Add test code
    
    * Add messages
    
    * Add Import statements
    
    ---------
    
    Co-authored-by: Christoph Hombach <Christoph.Hombach@2xideas.com>

---
+++ montrek/code_generation/management/commands/generate_dependency_table.py
@@ -0,0 +1,105 @@
+import os
+
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def __init__(self, stdout=None, stderr=None, no_color=False, force_color=False):
+        self.model_in: str = ""
+        self.model_out: str = ""
+        self.path_in: str = ""
+        self.path_out: str = ""
+        super().__init__(stdout, stderr, no_color, force_color)
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "path_in",
+            type=str,
+            help="Path to app in which hub_in is located.",
+        )
+        parser.add_argument(
+            "model_in",
+            type=str,
+            help="Model in in lowercase notation",
+        )
+        parser.add_argument(
+            "path_out",
+            type=str,
+            help="Path to app in which hub_out is located.",
+        )
+        parser.add_argument(
+            "model_out",
+            type=str,
+            help="Model out in lowercase notation",
+        )
+
+    def handle(self, *args, **kwargs):
+        self.model_out = kwargs["model_out"]
+        self.model_in = kwargs["model_in"]
+        self.model_in_name = self.get_model_name(self.model_in)
+        self.model_out_name = self.get_model_name(self.model_out)
+        self.path_in = kwargs["path_in"]
+        self.path_out = kwargs["path_out"]
+        self.python_path_in = self.get_python_path(self.path_in)
+        self.python_path_out = self.get_python_path(self.path_out)
+        call_command(
+            "generate_link", self.path_in, self.model_in, self.path_out, self.model_out
+        )
+        self.add_view_test()
+
+    def get_model_name(self, model: str) -> str:
+        return model.replace("_", " ").title().replace(" ", "")
+
+    def get_python_path(self, path: str) -> str:
+        if path.endswith(os.path.sep):
+            path = path[:-1]
+
+        return path.replace(os.path.sep, ".")
+
+    def _add_code(
+        self,
+        path: str,
+        search_string: str,
+        code: str,
+        import_statements: tuple[str, ...] = (),
+    ):
+        with open(path, "r") as f:
+            old_code = f.read()
+        if search_string in old_code:
+            self.stdout.write(f"{search_string} exists already in {path}!")
+            return
+        new_code = old_code + code
+        new_code = "\n".join(import_statements) + "\n" + new_code
+        with open(path, "w") as f:
+            f.write(new_code)
+        self.stdout.write(f"Created {search_string} in {path}!")
+
+    def add_view_test(self):
+        test_path = os.path.join(
+            self.path_out, "tests", "views", f"test_{self.model_out}_views.py"
+        )
+        class_name = f"Test{self.model_out_name}{self.model_in_name}sListView"
+        code = f"""class {class_name}(MontrekListViewTestCase):
+    viewname = "{self.model_out}_{self.model_in}s_list"
+    view_class = {self.model_out_name}{self.model_in_name}sListView
+    expected_no_of_rows = 5
+
+    def build_factories(self):
+        self.{self.model_out}_factory = {self.model_out_name}SatelliteFactory.create()
+        {self.model_in_name}SatelliteFactory.create_batch(
+            5, {self.model_out}=self.{self.model_out}_factory
+        )
+        other_{self.model_out}_factory = {self.model_out_name}SatelliteFactory.create()
+        {self.model_in_name}SatelliteFactory.create_batch(
+            5, {self.model_out}=other_{self.model_out}_factory
+        )
+
+    def url_kwargs(self):
+        return {{"pk": self.{self.model_out}_factory.get_hub_value_date().pk}}"""
+        import_statements = (
+            f"from {self.python_path_out}.views.{self.model_out}_views import {self.model_out_name}{self.model_in_name}sListView",
+            f"from {self.python_path_out}.tests.factories.{self.model_out}_sat_factories import {self.model_out_name}SatelliteFactory",
+            f"from {self.python_path_in}.tests.factories.{self.model_in}_sat_factories import {self.model_in_name}SatelliteFactory",
+        )
+        self._add_code(test_path, class_name, code, import_statements)
---
+++ montrek/code_generation/tests/management/commands/test_dependecy_table.py
@@ -0,0 +1,57 @@
+import io
+import os
+import shutil
+from unittest.mock import patch
+
+from code_generation.tests import get_test_file_path
+from django.core.management import call_command
+from django.test import TestCase
+
+
+class TestGenerateLinkCommand(TestCase):
+    def setUp(self):
+        self.maxDiff = None
+
+    def test_insertions_in_files(self):
+        self.output_dir = os.path.relpath(get_test_file_path("output_dependecy_table"))
+        os.makedirs(self.output_dir, exist_ok=True)
+        with patch("sys.stdout", new_callable=io.StringIO):
+            call_command("generate_table", self.output_dir, "mother")
+            call_command("generate_table", self.output_dir, "daughter")
+            call_command(
+                "generate_dependency_table",
+                self.output_dir,
+                "daughter",
+                self.output_dir,
+                "mother",
+            )
+        with open(
+            os.path.join(self.output_dir, "tests", "views", "test_mother_views.py")
+        ) as f:
+            code = f.read().replace(" ", "")
+            expected_code = """class TestMotherDaughtersListView(MontrekListViewTestCase):
+    viewname = "mother_daughters_list"
+    view_class = MotherDaughtersListView
+    expected_no_of_rows = 5
+
+    def build_factories(self):
+        self.mother_factory = MotherSatelliteFactory.create()
+        DaughterSatelliteFactory.create_batch(
+            5, mother=self.mother_factory
+        )
+        other_mother_factory = MotherSatelliteFactory.create()
+        DaughterSatelliteFactory.create_batch(
+            5, mother=other_mother_factory
+        )
+
+    def url_kwargs(self):
+        return {"pk": self.mother_factory.get_hub_value_date().pk}"""
+            self.assertIn(expected_code.replace(" ", ""), code)
+            import_statements = (
+                "from code_generation.tests.data.output_dependecy_table.views.mother_views import MotherDaughtersListView\n",
+                "from code_generation.tests.data.output_dependecy_table.tests.factories.mother_sat_factories import MotherSatelliteFactory\n",
+                "from code_generation.tests.data.output_dependecy_table.tests.factories.daughter_sat_factories import DaughterSatelliteFactory\n",
+            )
+            for import_statement in import_statements:
+                self.assertIn(import_statement.replace(" ", ""), code)
+        shutil.rmtree(self.output_dir)
