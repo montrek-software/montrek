c29841673c1248d563886e3640b37c5983bfe0fe
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-09-03 13:47:57 +0200

    Add test code

--- montrek/code_generation/management/commands/generate_dependency_table.py
+++ montrek/code_generation/management/commands/generate_dependency_table.py
@@ -46,6 +46,7 @@ class Command(BaseCommand):
         call_command(
             "generate_link", self.path_in, self.model_in, self.path_out, self.model_out
         )
+        self.add_view_test()
 
     def get_model_name(self, model: str) -> str:
         return model.replace("_", " ").title().replace(" ", "")
@@ -55,3 +56,36 @@ class Command(BaseCommand):
             path = path[:-1]
 
         return path.replace(os.path.sep, ".")
+
+    def _add_code(self, path: str, search_string: str, code: str):
+        with open(path, "r") as f:
+            old_code = f.read()
+        if search_string in old_code:
+            return
+        new_code = old_code + code
+        with open(path, "w") as f:
+            f.write(new_code)
+
+    def add_view_test(self):
+        test_path = os.path.join(
+            self.path_out, "tests", "views", f"test_{self.model_out}_views.py"
+        )
+        class_name = f"Test{self.model_out_name}{self.model_in_name}sListView"
+        code = f"""class {class_name}(vtc.MontrekListViewTestCase):
+    viewname = "{self.model_out}_{self.model_in}s_list"
+    view_class = views.{self.model_out_name}{self.model_in_name}sListView
+    expected_no_of_rows = 5
+
+    def build_factories(self):
+        self.{self.model_out}_factory = {self.model_out_name}SatelliteFactory.create()
+        {self.model_in_name}SatelliteFactory.create_batch(
+            5, {self.model_out}=self.{self.model_out}_factory
+        )
+        other_{self.model_out}_factory = {self.model_out_name}SatelliteFactory.create()
+        {self.model_in_name}SatelliteFactory.create_batch(
+            5, {self.model_out}=other_{self.model_out}_factory
+        )
+
+    def url_kwargs(self):
+        return {{"pk": self.{self.model_out}_factory.get_hub_value_date().pk}}"""
+        self._add_code(test_path, class_name, code)
--- montrek/code_generation/tests/management/commands/test_dependecy_table.py
+++ montrek/code_generation/tests/management/commands/test_dependecy_table.py
@@ -25,4 +25,26 @@ class TestGenerateLinkCommand(TestCase):
                 self.output_dir,
                 "mother",
             )
+        with open(
+            os.path.join(self.output_dir, "tests", "views", "test_mother_views.py")
+        ) as f:
+            code = f.read().replace(" ", "")
+            expected_code = """class TestMotherDaughtersListView(vtc.MontrekListViewTestCase):
+    viewname = "mother_daughters_list"
+    view_class = views.MotherDaughtersListView
+    expected_no_of_rows = 5
+
+    def build_factories(self):
+        self.mother_factory = MotherSatelliteFactory.create()
+        DaughterSatelliteFactory.create_batch(
+            5, mother=self.mother_factory
+        )
+        other_mother_factory = MotherSatelliteFactory.create()
+        DaughterSatelliteFactory.create_batch(
+            5, mother=other_mother_factory
+        )
+
+    def url_kwargs(self):
+        return {"pk": self.mother_factory.get_hub_value_date().pk}"""
+            self.assertIn(expected_code.replace(" ", ""), code)
         shutil.rmtree(self.output_dir)
