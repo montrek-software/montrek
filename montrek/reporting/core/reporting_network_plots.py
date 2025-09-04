f6f74d634cde939ff8c23f49dff85b754be044c3
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-09-04 15:13:08 +0200

    Fill plot with data

--- montrek/reporting/core/reporting_network_plots.py
+++ montrek/reporting/core/reporting_network_plots.py
@@ -1,5 +1,7 @@
 from typing import Any
 
+import networkx as nx
+import numpy as np
 from plotly.graph_objs import Scatter
 from reporting.core.reporting_data import ReportingNetworkData
 from reporting.core.reporting_plots import ReportingPlotBase
@@ -12,4 +14,46 @@ class ReportingNetworkPlot(ReportingPlotBase[ReportingNetworkData]):
     def _get_figure_data(
         self, _x: list[Any], reporting_data: ReportingNetworkData
     ) -> list[Scatter]:
-        return []
+        graph = reporting_data.graph
+        pos = self.grouped_grid_layout(reporting_data)
+        edge_trace = self.get_edges(pos, graph)
+        node_trace = self.get_nodes(pos, graph)
+        return [edge_trace, node_trace]
+
+    def grouped_grid_layout(
+        self, reporting_data: ReportingNetworkData
+    ) -> dict[str, np.ndarray]:
+        graph = reporting_data.graph
+        if graph.number_of_nodes() == 0:
+            return {}
+        return nx.spring_layout(graph, seed=42, dim=2)
+
+    def get_edges(self, pos: dict[str, np.ndarray], graph: nx.DiGraph) -> Scatter:
+        edge_x, edge_y = [], []
+        for src, dst in graph.edges():
+            x0, y0 = pos[src]
+            x1, y1 = pos[dst]
+            edge_x += [x0, x1, None]
+            edge_y += [y0, y1, None]
+
+        return Scatter(
+            x=edge_x,
+            y=edge_y,
+            line=dict(width=1, color="#888"),
+            mode="lines",
+            hoverinfo="none",
+        )
+
+    def get_nodes(self, pos: dict[str, np.ndarray], graph: nx.DiGraph) -> Scatter:
+        node_x, node_y = (
+            [],
+            [],
+        )
+        for node, attrs in graph.nodes(data=True):
+            x, y = pos[node]
+            node_x.append(x)
+            node_y.append(y)
+        return Scatter(
+            x=node_x,
+            y=node_y,
+        )
--- montrek/reporting/tests/core/test_reporting_network_plots.py
+++ montrek/reporting/tests/core/test_reporting_network_plots.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from networkx import DiGraph
+from plotly.graph_objs import Scatter
 from reporting.core.reporting_data import ReportingNetworkData
 from reporting.core.reporting_network_plots import ReportingNetworkPlot
 
@@ -9,8 +10,12 @@ class TestReportingNetworkPlot(TestCase):
         graph = DiGraph()
         graph.add_node("A")
         graph.add_node("B")
+        graph.add_edge("A", "B")
 
         reporting_data = ReportingNetworkData(graph=graph, title="Test Network Graph")
 
         reporting_plot = ReportingNetworkPlot()
         reporting_plot.generate(reporting_data)
+        self.assertTrue(
+            all([isinstance(plot, Scatter) for plot in reporting_plot.figure.data])
+        )
