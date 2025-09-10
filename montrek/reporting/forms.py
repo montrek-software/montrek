cfb26fc890cf0924b8c7e9e1b4fb2e192572deb5
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-09-10 14:28:14 +0200

    render template

--- montrek/reporting/forms.py
+++ montrek/reporting/forms.py
@@ -1,13 +1,43 @@
+import os
+
+from django.conf import settings
 from django.forms import Form
+from django.template import Context, Template
 
 
 class MontrekReportForm(Form):
-    template: str | None = None
+    form_template: str | None = None
 
     def to_html(self) -> str:
-        if not self.template:
+        context = Context({"form": self})
+        template = Template(self.read_template())
+        rendered_template = template.render(context)
+        return f"""
+    <form method="post">
+    {{% csrf_token %}}
+    {rendered_template}
+    <button type="submit">Submit</button>
+    </form>
+            """
+
+    def read_template(self) -> str:
+        if not self.form_template:
             raise NotImplementedError("MontrekReportForm needs template attribute")
-        return ""
+        template_path = self._get_template_path()
+        with open(template_path, "r") as file:
+            return file.read()
+
+    def _get_template_path(self) -> str:
+        for template_dir in settings.TEMPLATES[0]["DIRS"]:
+            potential_path = os.path.join(
+                settings.BASE_DIR,
+                template_dir,
+                "report_form_templates",
+                str(self.form_template),
+            )
+            if os.path.exists(potential_path):
+                return potential_path
+        raise FileNotFoundError(f"Template {self.form_template} not found")
 
 
 class NoMontrekReportForm(MontrekReportForm):
--- montrek/reporting/tests/test_forms.py
+++ montrek/reporting/tests/test_forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.test import TestCase
 from reporting.forms import MontrekReportForm
 
@@ -5,9 +6,32 @@ from reporting.forms import MontrekReportForm
 class MockNoTemplateMontrekReportForm(MontrekReportForm): ...
 
 
+class MockTemplateNotFoundMontrekReportForm(MontrekReportForm):
+    form_template = "not_found"
+
+
+class MockMontrekReportForm(MontrekReportForm):
+    form_template = "test_form.html"
+    field_1 = forms.CharField()
+
+
 class TestMontrekReportForm(TestCase):
     def test_no_template(self):
         self.assertRaises(
             NotImplementedError,
             MockNoTemplateMontrekReportForm().to_html,
+        )
+
+    def test_template_not_found(self):
+        self.assertRaises(
+            FileNotFoundError,
+            MockTemplateNotFoundMontrekReportForm().to_html,
+        )
+
+    def test_renter_template(self):
+        form = MockMontrekReportForm()
+        test_html = form.to_html()
+        self.assertEqual(
+            test_html,
+            '\n    <form method="post">\n    {% csrf_token %}\n    <fieldset>\n  <legend>Test</legend>\n  <input type="text" name="field_1" required id="id_field_1">\n</fieldset>\n\n    <button type="submit">Submit</button>\n    </form>\n            ',
         )
