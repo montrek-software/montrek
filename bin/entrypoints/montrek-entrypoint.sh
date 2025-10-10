659221bed5e922bb8b58ef734c440b449e7a527f
chrishombach <chris.hombach@gmail.com>
2025-10-10 14:32:51 +0200

    Add fast worker

--- bin/entrypoints/montrek-entrypoint.sh
+++ bin/entrypoints/montrek-entrypoint.sh
@@ -6,6 +6,7 @@ echo "SERVICE is: $SERVICE"
 
 # Conditionally run the app as appuser only for the "web" role
 echo "Running $SERVICE logic..."
+echo "${PUID}:${PGID}"
 case "$SERVICE" in
 web)
   exec gosu "${PUID}:${PGID}" /montrek/bin/entrypoints/web-entrypoint.sh
@@ -15,6 +16,9 @@ sequential_worker)
   ;;
 parallel_worker)
   exec gosu "${PUID}:${PGID}" /montrek/bin/entrypoints/worker-entrypoint.sh 5
+  ;;
+fast_worker)
+  exec gosu "${PUID}:${PGID}" /montrek/bin/entrypoints/worker-entrypoint.sh 2
   ;;
 celery_beat)
   exec gosu "${PUID}:${PGID}" /montrek/bin/entrypoints/celery_beat-entrypoint.sh
