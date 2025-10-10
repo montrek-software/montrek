e6fe2e6db5111409161fa1024663017ee0c7c9f3
chrishombach <chris.hombach@gmail.com>
2025-10-10 15:09:55 +0200

    Remove fixed concurrency

--- bin/entrypoints/montrek-entrypoint.sh
+++ bin/entrypoints/montrek-entrypoint.sh
@@ -12,13 +12,13 @@ web)
   exec gosu "${PUID}:${PGID}" /montrek/bin/entrypoints/web-entrypoint.sh
   ;;
 sequential_worker)
-  exec gosu "${PUID}:${PGID}" /montrek/bin/entrypoints/worker-entrypoint.sh 1
+  exec gosu "${PUID}:${PGID}" /montrek/bin/entrypoints/worker-entrypoint.sh
   ;;
 parallel_worker)
-  exec gosu "${PUID}:${PGID}" /montrek/bin/entrypoints/worker-entrypoint.sh 5
+  exec gosu "${PUID}:${PGID}" /montrek/bin/entrypoints/worker-entrypoint.sh
   ;;
 fast_worker)
-  exec gosu "${PUID}:${PGID}" /montrek/bin/entrypoints/worker-entrypoint.sh 2
+  exec gosu "${PUID}:${PGID}" /montrek/bin/entrypoints/worker-entrypoint.sh
   ;;
 celery_beat)
   exec gosu "${PUID}:${PGID}" /montrek/bin/entrypoints/celery_beat-entrypoint.sh
