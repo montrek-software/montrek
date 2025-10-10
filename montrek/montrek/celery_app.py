34c4fdb029138d124727591b6a8d2399c1c78980
chrishombach <chris.hombach@gmail.com>
2025-10-10 14:49:33 +0200

    Add Fast Queue

--- montrek/montrek/celery_app.py
+++ montrek/montrek/celery_app.py
@@ -5,6 +5,7 @@ from kombu import Queue
 
 SEQUENTIAL_QUEUE_NAME = "sequential_queue"
 PARALLEL_QUEUE_NAME = "parallel_queue"
+FAST_QUEUE_NAME = "fast_queue"
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "montrek.settings")
 app = Celery(
@@ -14,6 +15,7 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 app.conf.task_queues = (
     Queue(SEQUENTIAL_QUEUE_NAME, routing_key=f"{SEQUENTIAL_QUEUE_NAME}.#"),
     Queue(PARALLEL_QUEUE_NAME, routing_key=f"{PARALLEL_QUEUE_NAME}.#"),
+    Queue(FAST_QUEUE_NAME, routing_key=f"{FAST_QUEUE_NAME}.#"),
 )
 
 app.conf.task_default_queue = SEQUENTIAL_QUEUE_NAME
