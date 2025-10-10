8cab43bef167198f421b1f15874fa81af9f48ca5
chrishombach <chris.hombach@gmail.com>
2025-10-10 15:09:03 +0200

    Set Concurrency based on CPUs

--- bin/entrypoints/worker-entrypoint.sh
+++ bin/entrypoints/worker-entrypoint.sh
@@ -2,5 +2,27 @@
 set -e
 
 . .venv/bin/activate
+# Detect CPU count (not super relevant, but can guide concurrency cap)
+CPU_COUNT=$(nproc --all 2>/dev/null || echo 4)
+case "$SERVICE" in
+sequential_worker)
+  CONCURRENCY=1
+  ;;
+parallel_worker)
+  CONCURRENCY=$((CPU_COUNT - 2))
+  ;;
+fast_worker)
+  CONCURRENCY=$((CPU_COUNT * 50))
+  ;;
+*)
+  echo "Unknown service $SERVICE"
+  ;;
+esac
+# Cap to avoid extreme values
+MAX_CONCURRENCY=500
+if [[ $CONCURRENCY -gt $MAX_CONCURRENCY ]]; then
+  CONCURRENCY=$MAX_CONCURRENCY
+fi
+
 cd montrek
-python -m celery --app=montrek worker --loglevel=info -Q $QUEUE --concurrency=$1 -n $SERVICE@%h --pool=$POOL
+python -m celery --app=montrek worker --loglevel=info -Q $QUEUE --concurrency=$CONCURRENCY -n $SERVICE@%h --pool=$POOL
