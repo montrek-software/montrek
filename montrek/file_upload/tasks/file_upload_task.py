e7bfd726b4012e4dfda85db61dd06782ee7e1655
chrishombach <chris.hombach@gmail.com>
2025-11-10 10:42:59 +0100

    Refactor class

--- montrek/file_upload/tasks/file_upload_task.py
+++ montrek/file_upload/tasks/file_upload_task.py
@@ -1,8 +1,8 @@
 import logging
-from typing import Any, Dict
 from urllib.parse import urlparse
 
 from baseclasses.managers.montrek_manager import MontrekManager
+from baseclasses.typing import SessionDataType
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.http import HttpResponseRedirect
@@ -16,6 +16,10 @@ logger = logging.getLogger(__name__)
 
 
 class FileUploadTask(MontrekTask):
+    success_message = "Background file processing finished successfully."
+    failure_message = "ERROR: Background file processing did not finish successfully!"
+    mailing_manager_class: type[MailingManager] = MailingManager
+
     def __init__(
         self,
         manager_class: type[MontrekManager],
@@ -27,21 +31,27 @@ class FileUploadTask(MontrekTask):
         )
         super().__init__(task_name, queue)
 
-    def run(self, session_data: Dict[str, Any]):
+    def run(self, session_data: SessionDataType):
         logger.debug("Start task run")
         manager = self.manager_class(session_data)
         logger.debug("%s: manager: %s", self.__class__.__name__, manager)
         result = manager.process()
+        message = self.send_result_mail(manager, session_data, result)
+        logger.debug("End task run")
+        return message
+
+    def send_result_mail(
+        self, manager: MontrekManager, session_data: SessionDataType, result: bool
+    ) -> str:
         message = manager.processor.message
         user = get_user_model().objects.get(pk=session_data["user_id"])
         if result:
-            subject = "Background file processing finished successfully."
+            subject = self.success_message
         else:
-            subject = "ERROR: Background file processing did not finish successfully!"
-        MailingManager(session_data=session_data).send_montrek_mail(
+            subject = self.failure_message
+        self.mailing_manager_class(session_data=session_data).send_montrek_mail(
             user.email, subject, message
         )
-        logger.debug("End task run")
         return message
 
 
