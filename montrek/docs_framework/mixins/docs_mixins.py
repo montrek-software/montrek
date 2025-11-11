916c9e28311ae4b18936f6c9d3d1250a518133e6
chrishombach <chris.hombach@gmail.com>
2025-11-11 17:14:53 +0100

    Collect docs paths

--- montrek/docs_framework/mixins/docs_mixins.py
+++ montrek/docs_framework/mixins/docs_mixins.py
@@ -1,9 +1,25 @@
+import os
 import sys
 from pathlib import Path
 
 
 class DocsFilesMixin:
+    allowed_file_types = [".md"]
+
     def get_docs_path(self) -> Path:
         module = sys.modules[self.__class__.__module__]
         module_file = Path(str(module.__file__)).resolve()
         return module_file.parent.parent / "docs"
+
+    def get_docs_files(self) -> list[Path]:
+        docs_path = self.get_docs_path()
+        if not docs_path.exists():
+            return []
+
+        docs_files = []
+        for filename in os.listdir(docs_path):
+            file_path = docs_path / filename
+            if file_path.is_file() and file_path.suffix in self.allowed_file_types:
+                docs_files.append(file_path)
+
+        return docs_files
--- montrek/docs_framework/tests/mixins/test_docs_mixins.py
+++ montrek/docs_framework/tests/mixins/test_docs_mixins.py
@@ -1,8 +1,16 @@
+import os
+from pathlib import Path
+
 from django.test import TestCase
 from docs_framework.mixins.docs_mixins import DocsFilesMixin
 
 
 class MockTestClass(DocsFilesMixin): ...
+
+
+class MockTestClassNoDocsPath(DocsFilesMixin):
+    def get_docs_path(self):
+        return Path("unknown/docs")
 
 
 class TestDocsFilesMixin(TestCase):
@@ -13,3 +21,15 @@ class TestDocsFilesMixin(TestCase):
         test_docs_path = self.mixin.get_docs_path()
         dir_end = "/docs_framework/tests/docs"
         self.assertTrue(str(test_docs_path).endswith(dir_end))
+
+    def test_get_docs_files(self):
+        test_docs_files = self.mixin.get_docs_files()
+        self.assertEqual(len(test_docs_files), 3)
+        files_list = [str(docfile).split(os.sep)[-1] for docfile in test_docs_files]
+        files_list.sort()
+        self.assertEqual(files_list, ["docs_1.md", "docs_2.md", "docs_3.md"])
+
+    def test_get_docs_files__no_docs_path(self):
+        mixin = MockTestClassNoDocsPath()
+        test_docs_files = mixin.get_docs_files()
+        self.assertEqual(test_docs_files, [])
