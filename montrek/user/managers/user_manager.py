e833235de6c9e645d28ec7b99f01adeae5ba1d16
chrishombach <chris.hombach@gmail.com>
2025-11-17 11:45:11 +0100

    Get type right

--- montrek/user/managers/user_manager.py
+++ montrek/user/managers/user_manager.py
@@ -1,8 +1,9 @@
 from baseclasses.managers.montrek_manager import MontrekManager
 from django.contrib.auth import get_user_model
+from user.models import MontrekUser
 
 
 class UserManager(MontrekManager):
-    def get_superuser(self) -> None:
+    def get_superuser(self) -> None | MontrekUser:
         user_model = get_user_model()
         return user_model.objects.filter(is_superuser=True).first()
