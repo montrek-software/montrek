2deee4b8a77868f140463435d50e140950879542
chrishombach <chris.hombach@gmail.com>
2025-12-19 16:03:39 +0100

    Implement cache factory return

--- montrek/baseclasses/repositories/db/db_creator_cache.py
+++ montrek/baseclasses/repositories/db/db_creator_cache.py
@@ -1,3 +1,15 @@
+from typing import Protocol
+
+
+class DbCreatorCacheProtocol(Protocol): ...
+
+
+class DbCreatorCacheBlank(DbCreatorCacheProtocol): ...
+
+
 class DbCreatorCacheFactory:
     def __init__(self, columns: list[str]):
         self.columns = columns
+
+    def get_cache(self) -> DbCreatorCacheProtocol:
+        return DbCreatorCacheBlank()
--- montrek/baseclasses/tests/repositories/db/test_db_creator_cache.py
+++ montrek/baseclasses/tests/repositories/db/test_db_creator_cache.py
@@ -1,7 +1,11 @@
-from baseclasses.repositories.db.db_creator_cache import DbCreatorCacheFactory
+from baseclasses.repositories.db.db_creator_cache import (
+    DbCreatorCacheBlank,
+    DbCreatorCacheFactory,
+)
 from django.test import TestCase
 
 
 class TestDbCreatorCacheFactory(TestCase):
     def test_get_no_hub_id_cache(self):
         cache_factory = DbCreatorCacheFactory(["field_1", "field_2"])
+        self.assertIsInstance(cache_factory.get_cache(), DbCreatorCacheBlank)
