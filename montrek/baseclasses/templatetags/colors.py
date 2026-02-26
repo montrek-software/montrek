6ac47613b8f87c7dbe9345872d2682da92973efe
chrishombach <chris.hombach@gmail.com>
2026-02-26 11:42:00 +0100

    Refactor get_color to cache color_schema

--- montrek/baseclasses/templatetags/colors.py
+++ montrek/baseclasses/templatetags/colors.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 from django import template
 from django.conf import settings
 from reporting.core.reporting_colors import Color, ReportingColors
@@ -5,14 +7,18 @@ from reporting.core.reporting_colors import Color, ReportingColors
 register = template.Library()
 
 
-@register.simple_tag
-def get_color(name):
+@cache
+def _get_color_scheme():
     primary_color = Color("primary", settings.PRIMARY_COLOR)
     secondary_color = Color("secondary", settings.SECONDARY_COLOR)
-    color_scheme = {
+    return {
         "primary": primary_color.hex,
         "secondary": secondary_color.hex,
+        "primary_light": ReportingColors.lighten_color(primary_color).hex,
+        "secondary_light": ReportingColors.lighten_color(secondary_color).hex,
     }
-    color_scheme["primary_light"] = ReportingColors.lighten_color(primary_color).hex
-    color_scheme["secondary_light"] = ReportingColors.lighten_color(secondary_color).hex
-    return color_scheme.get(name, "#000000")
+
+
+@register.simple_tag
+def get_color(name):
+    return _get_color_scheme().get(name, "#000000")
