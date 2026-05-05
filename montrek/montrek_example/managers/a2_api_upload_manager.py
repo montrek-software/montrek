02f5a19d708fbc9fd06b2cd8188e210ad9055029
chrishombach <chris.hombach@gmail.com>
2026-05-05 13:27:38 +0200

    Set example api right

--- montrek/montrek_example/managers/a2_api_upload_manager.py
+++ montrek/montrek_example/managers/a2_api_upload_manager.py
@@ -2,12 +2,9 @@ from data_import.api_import.managers.api_data_import_manager import ApiDataImpor
 from montrek_example.managers.a2_api_upload_processor import (
     A2ApiUploadProcessor,
 )
-from montrek_example.managers.a2_request_manager import A2RequestManager
 from montrek_example.repositories.hub_a_repository import HubAApiUploadRepository
 
 
 class A2ApiUploadManager(ApiDataImportManager):
     registry_repository_class = HubAApiUploadRepository
-    endpoint = "a2_endpoint"
-    request_manager_class = A2RequestManager
     processor_class = A2ApiUploadProcessor
