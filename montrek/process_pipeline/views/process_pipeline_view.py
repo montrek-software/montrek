4bd78e2c9d4a297f8835132a563590770b80652d
chrishombach <chris.hombach@gmail.com>
2026-05-06 16:30:58 +0200

    Add external get_success_url method (#1500)

--- montrek/process_pipeline/views/process_pipeline_view.py
+++ montrek/process_pipeline/views/process_pipeline_view.py
@@ -5,10 +5,13 @@ from baseclasses.views import MontrekRedirectView
 class ProcessPipelineViewABC(MontrekRedirectView):
     success_url = "under_construction"
 
+    def get_success_url(self) -> str:
+        return reverse(self.success_url)
+
     def get_redirect_url(self, *args, **kwargs) -> str:
         self.process()
         self.show_messages()
-        return reverse(self.success_url)
+        return self.get_success_url()
 
     def process(self):
         raise NotImplementedError("Implement process method")
