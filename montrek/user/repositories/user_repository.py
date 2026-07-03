f9e5ae6d1e6042454b5888a6d900034e8bd0b1ab
chrishombach <chris.hombach@gmail.com>
2026-07-03 15:43:27 +0200

    ADd User repository (#1629)

--- montrek/user/repositories/user_repository.py
+++ montrek/user/repositories/user_repository.py
@@ -1,6 +1,14 @@
-from user.models import MontrekUser
+from baseclasses.repositories.montrek_repository import MontrekRepository
+from user.models import MontrekUser, UserAssignmentHub, UserAssignmentSatellite
 
 
 class MontrekUserRepository:
     def receive(self):
         return MontrekUser.objects.all()
+
+
+class UserAssignmentRepository(MontrekRepository):
+    hub_class = UserAssignmentHub
+
+    def set_annotations(self):
+        self.add_satellite_fields_annotations(UserAssignmentSatellite, ["user__email"])
