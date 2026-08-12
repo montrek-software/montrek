3deae6f6333cfc2878fca9ede56fd221e0c2d077
chrishombach <chris.hombach@gmail.com>
2026-08-12 14:09:09 +0200

    Reduce queries

--- montrek/user/managers/user_manager.py
+++ montrek/user/managers/user_manager.py
@@ -4,9 +4,9 @@ from user.models import MontrekUser
 
 
 class UserManager(MontrekManager):
-    def get_superuser(self) -> None | MontrekUser:
+    def get_superuser(self) -> MontrekUser:
         user_model = get_user_model()
-        super_user = user_model.objects.filter(is_superuser=True)
-        if not super_user.exists():
+        super_user = user_model.objects.filter(is_superuser=True).first()
+        if super_user is None:
             raise ValueError("No superuser available!")
-        return super_user.first()
+        return super_user
