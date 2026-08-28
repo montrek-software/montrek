a094e049289b88f8d72f3470526a8224fd0a67a1
chrishombach <chris.hombach@gmail.com>
2026-08-28 10:32:42 +0200

    Set colors

--- montrek/file_export/constants.py
+++ montrek/file_export/constants.py
@@ -2,10 +2,11 @@ from process_pipeline.constants import (
     RegistryStatusDataClass,
     RegistryStatusTextChoices,
 )
+from reporting.core.reporting_colors import ReportingColors
 
 
 class ExportStatus(RegistryStatusTextChoices):
-    PENDING = RegistryStatusDataClass("pending")
-    IN_PROGRESS = RegistryStatusDataClass("in_progress")
-    PROCESSED = RegistryStatusDataClass("processed")
-    FAILED = RegistryStatusDataClass("failed")
+    PENDING = RegistryStatusDataClass("pending", ReportingColors.BLUE)
+    IN_PROGRESS = RegistryStatusDataClass("in_progress", ReportingColors.YELLOW)
+    PROCESSED = RegistryStatusDataClass("processed", ReportingColors.DARK_GREEN)
+    FAILED = RegistryStatusDataClass("failed", ReportingColors.RED)
--- montrek/file_upload/constants.py
+++ montrek/file_upload/constants.py
@@ -2,12 +2,13 @@ from process_pipeline.constants import (
     RegistryStatusDataClass,
     RegistryStatusTextChoices,
 )
+from reporting.core.reporting_colors import ReportingColors
 
 
 class UploadStatus(RegistryStatusTextChoices):
-    PENDING = RegistryStatusDataClass("pending")
-    UPLOADED = RegistryStatusDataClass("uploaded")
-    IN_PROGRESS = RegistryStatusDataClass("in_progress")
-    PROCESSED = RegistryStatusDataClass("processed")
-    FAILED = RegistryStatusDataClass("failed")
-    REVOKED = RegistryStatusDataClass("revoked")
+    PENDING = RegistryStatusDataClass("pending", ReportingColors.BLUE)
+    UPLOADED = RegistryStatusDataClass("uploaded", ReportingColors.DARK_GREEN)
+    IN_PROGRESS = RegistryStatusDataClass("in_progress", ReportingColors.YELLOW)
+    PROCESSED = RegistryStatusDataClass("processed", ReportingColors.DARK_GREEN)
+    FAILED = RegistryStatusDataClass("failed", ReportingColors.RED)
+    REVOKED = RegistryStatusDataClass("revoked", ReportingColors.RED)
